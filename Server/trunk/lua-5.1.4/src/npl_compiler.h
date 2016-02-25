8b781036006129120d0599fdbb617b23602026c4
LiXizhi <lixizhi@yeah.net>
2016-02-25 17:28:04 +0800

    third party libs

---
+++ Server/trunk/lua-5.1.4/src/npl_compiler.h
@@ -0,0 +1,10 @@
+#pragma once
+//-----------------------------------------------------------------------------
+// Class:	NPL Compiler
+// Authors:	LiXizhi
+// Emails:	LiXizhi@yeah.net
+// Company: ParaEngine
+// Date:	2010.3.15
+//-----------------------------------------------------------------------------
+
+LUA_API int npl_compile(int argc, char* argv[]);
---
+++ Server/trunk/sqlite-3.6.23.1/paraengine_sqlite3.h
@@ -0,0 +1,19 @@
+#pragma once
+//-----------------------------------------------------------------------------
+// Copyright (C) 2004 - 2010 ParaEngine Co., All Rights Reserved.
+// Author: LiXizhi
+// Date:	2010.4
+// Description: This will compile sqlite3 as dll in win32.
+//-----------------------------------------------------------------------------
+#ifdef WIN32
+
+#ifdef PE_CORE_EXPORTING
+#define SQLITE_API    __declspec(dllimport)
+#else
+#define SQLITE_API    __declspec(dllexport)
+#endif
+
+#else
+
+#endif
+
