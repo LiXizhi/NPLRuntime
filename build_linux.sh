59c7ce66e2d4bfbf39fe91b3f73994fe41639ef5
LiXizhi <lixizhi@yeah.net>
2016-02-26 15:59:43 +0800

    travis fix

--- build_linux.sh
+++ build_linux.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-olddir=`pwd`
+# author: lixizhi@yeah.net
+# date: 2016.2.26
+# desc: for install dependencies, please see `.travis.xml` 
+
+pushd .
 mkdir -p ./bin/linux
 cd bin/linux/
 cmake ../../NPLRuntime/ && make -j3
 result=$?
-
-cd $olddir
+popd
 
 exit $result
