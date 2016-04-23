53f1c6ffc200049f71ccaf30c2a01020c74a2089
LiXizhi <LiXizhi@yeah.net>
2016-04-24 00:11:07 +0800

    added curl and zlib for win32 build

--- build_win32.bat
+++ build_win32.bat
@@ -1,7 +1,9 @@
 rem author: lixizhi@yeah.net
 rem date: 2016.2.26
 rem desc: to install dependencies, please see `.travis.xml`
-
+rem guide: add `BOOST_ROOT` to environment variable, such as 'D:\lxzsrc\NPLRuntime\Server\trunk\boost_1_60_0', make sure to prebuilt your boost library with `./b2`
+rem build: open `NPLRuntime/cmakelist.txt` with cmake-gui or run this bat file from visual c++ env command line.
+  
 pushd .
 mkdir bin\win32
 cd bin\win32
