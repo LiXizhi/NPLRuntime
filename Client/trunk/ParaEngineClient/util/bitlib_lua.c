eacfb3a83c61548dfbaaea1f04d0b45e6205517f
LXZ_svn_sync <lixizhi@yeah.net>
2016-06-17 16:32:47 +0800

    svn_sync

--- Client/trunk/ParaEngineClient/util/bitlib_lua.c
+++ Client/trunk/ParaEngineClient/util/bitlib_lua.c
@@ -1,114 +1,197 @@
 //-----------------------------------------------------------------------------
 // Class:	bit.*** in NPL
-// Authors:	ported by LiXizhi, based on Bitwise operations library by Reuben Thomas 2000-2008
+// Authors:	ported by LiXizhi
 // Emails:	LiXizhi@yeah.net
 // Date:	2008.5.10
 // Desc: Port note: the test is passed. "stdint.h" is added for this application from the web. 
 // A modified lua 5.1.3 core to allow string.format("%x") to work for entire 52 bits, instead of 31 bits in the official release
 //-----------------------------------------------------------------------------
-/* Bitwise operations library */
-/* (c) Reuben Thomas 2000-2008 */
+/*
+** Lua BitOp -- a bit operations library for Lua 5.1/5.2.
+** http://bitop.luajit.org/
+**
+** Copyright (C) 2008-2012 Mike Pall. All rights reserved.
+**
+** Permission is hereby granted, free of charge, to any person obtaining
+** a copy of this software and associated documentation files (the
+** "Software"), to deal in the Software without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Software, and to
+** permit persons to whom the Software is furnished to do so, subject to
+** the following conditions:
+**
+** The above copyright notice and this permission notice shall be
+** included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+** SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**
+** [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+*/
 
-#include <lua.h>
-#include <lauxlib.h>
-#include <limits.h>
+#define LUA_BITOP_VERSION	"1.0.2"
 
-#define BUILTIN_CAST
+#define LUA_LIB
+#include "lua.h"
+#include "lauxlib.h"
 
-
-#define BITLIB_FLOAT_BITS 53
-#define BITLIB_FLOAT_MAX  0xfffffffffffffL
-#define BITLIB_FLOAT_MIN  (-0x10000000000000L)
-#define BITLIB_FLOAT_UMAX 0x1fffffffffffffUL
-
-#define BIT_BITS                                                        \
-	(CHAR_BIT * sizeof(lua_Integer) > BITLIB_FLOAT_BITS ?                 \
-BITLIB_FLOAT_BITS : (CHAR_BIT * sizeof(lua_Integer)))
-
-
-#ifdef BUILTIN_CAST
-/* FIXME: Should really use limits of lua_Integer (currently not given
-by Lua); the code below assumes that lua_Integer is ptrdiff_t, that
-size_t is the same as unsigned ptrdiff_t, that lua_Integer fits in
-a long (constants) and that lua_Number is floating-point and fits
-in a double (use of fmod). */
-#define TOINTEGER(L, n, f)                      \
-	((void)(f), luaL_checkinteger((L), (n)))
-#else
-#ifdef WIN32
-#include "stdint.h"
+#ifdef _MSC_VER
+/* MSVC is stuck in the last century and doesn't have C99's stdint.h. */
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
 #else
 #include <stdint.h>
 #endif
 
-/* This code may give warnings if BITLIB_FLOAT_* are too big to fit in
-long, but that doesn't matter since in that case they won't be
-used. */
-#define BIT_MAX                                                         \
-	(CHAR_BIT * sizeof(lua_Integer) > BITLIB_FLOAT_BITS ? BITLIB_FLOAT_MAX : PTRDIFF_MAX)
+typedef int32_t SBits;
+typedef uint32_t UBits;
 
-#define BIT_MIN                                                         \
-	(CHAR_BIT * sizeof(lua_Integer) > BITLIB_FLOAT_BITS ? BITLIB_FLOAT_MIN : PTRDIFF_MIN)
-
-#define BIT_UMAX                                                        \
-	(CHAR_BIT * sizeof(lua_Integer) > BITLIB_FLOAT_BITS ? BITLIB_FLOAT_UMAX : SIZE_MAX)
-
-#define TOINTEGER(L, n, f)                                              \
-	((ptrdiff_t)(((f) = fmod(luaL_checknumber((L), (n)), (double)BIT_UMAX + 1.0)), \
-	(f) > BIT_MAX ? ((f) -= (double)BIT_UMAX + 1) :          \
-	((f) < BIT_MIN ? ((f) += (double)BIT_UMAX + 1) : (f))))
+typedef union {
+	lua_Number n;
+#ifdef LUA_NUMBER_DOUBLE
+	uint64_t b;
+#else
+	UBits b;
 #endif
+} BitNum;
 
-#define TDYADIC(name, op, ty)                             \
-	static int bit_ ## name(lua_State *L) {                 \
-	lua_Number f;                                         \
-	lua_Integer w = TOINTEGER(L, 1, f);                   \
-	lua_pushinteger(L, (ty)w op TOINTEGER(L, 2, f));      \
-	return 1;                                             \
+/* Convert argument to bit type. */
+static UBits barg(lua_State *L, int idx)
+{
+	BitNum bn;
+	UBits b;
+#if LUA_VERSION_NUM < 502
+	bn.n = lua_tonumber(L, idx);
+#else
+	bn.n = luaL_checknumber(L, idx);
+#endif
+#if defined(LUA_NUMBER_DOUBLE)
+	bn.n += 6755399441055744.0;  /* 2^52+2^51 */
+#ifdef SWAPPED_DOUBLE
+	b = (UBits)(bn.b >> 32);
+#else
+	b = (UBits)bn.b;
+#endif
+#elif defined(LUA_NUMBER_INT) || defined(LUA_NUMBER_LONG) || \
+      defined(LUA_NUMBER_LONGLONG) || defined(LUA_NUMBER_LONG_LONG) || \
+      defined(LUA_NUMBER_LLONG)
+	if (sizeof(UBits) == sizeof(lua_Number))
+		b = bn.b;
+	else
+		b = (UBits)(SBits)bn.n;
+#elif defined(LUA_NUMBER_FLOAT)
+#error "A 'float' lua_Number type is incompatible with this library"
+#else
+#error "Unknown number type, check LUA_NUMBER_* in luaconf.h"
+#endif
+#if LUA_VERSION_NUM < 502
+	if (b == 0 && !lua_isnumber(L, idx)) {
+		luaL_typerror(L, idx, "number");
 	}
+#endif
+	return b;
+}
 
-#define MONADIC(name, op)                                 \
-	static int bit_ ## name(lua_State *L) {                 \
-	lua_Number f;                                         \
-	lua_pushinteger(L, op TOINTEGER(L, 1, f));            \
-	return 1;                                             \
-	}
+/* Return bit type. */
+#define BRET(b)  lua_pushnumber(L, (lua_Number)(SBits)(b)); return 1;
 
-#define VARIADIC(name, op)                      \
-	static int bit_ ## name(lua_State *L) {       \
-	lua_Number f;                               \
-	int n = lua_gettop(L), i;                   \
-	lua_Integer w = TOINTEGER(L, 1, f);         \
-	for (i = 2; i <= n; i++)                    \
-	w op TOINTEGER(L, i, f);                  \
-	lua_pushinteger(L, w);                      \
-	return 1;                                   \
-	}
+static int bit_tobit(lua_State *L) { BRET(barg(L, 1)) }
+static int bit_bnot(lua_State *L) { BRET(~barg(L, 1)) }
 
-MONADIC(cast,    +)
-MONADIC(bnot,    ~)
-VARIADIC(band,   &=)
-VARIADIC(bor,    |=)
-VARIADIC(bxor,   ^=)
-TDYADIC(lshift,  <<, lua_Integer)
-TDYADIC(rshift,  >>, size_t)
-TDYADIC(arshift, >>, lua_Integer)
+#define BIT_OP(func, opr) \
+  static int func(lua_State *L) { int i; UBits b = barg(L, 1); \
+    for (i = lua_gettop(L); i > 1; i--) b opr barg(L, i); BRET(b) }
+BIT_OP(bit_band, &= )
+BIT_OP(bit_bor, |= )
+BIT_OP(bit_bxor, ^= )
 
-static const struct luaL_reg bitlib[] = {
-	{"cast",    bit_cast},
-	{"bnot",    bit_bnot},
-	{"band",    bit_band},
-	{"bor",     bit_bor},
-	{"bxor",    bit_bxor},
-	{"lshift",  bit_lshift},
-	{"rshift",  bit_rshift},
-	{"arshift", bit_arshift},
-	{NULL, NULL}
-};
+#define bshl(b, n)  (b << n)
+#define bshr(b, n)  (b >> n)
+#define bsar(b, n)  ((SBits)b >> n)
+#define brol(b, n)  ((b << n) | (b >> (32-n)))
+#define bror(b, n)  ((b << (32-n)) | (b >> n))
+#define BIT_SH(func, fn) \
+  static int func(lua_State *L) { \
+    UBits b = barg(L, 1); UBits n = barg(L, 2) & 31; BRET(fn(b, n)) }
+BIT_SH(bit_lshift, bshl)
+BIT_SH(bit_rshift, bshr)
+BIT_SH(bit_arshift, bsar)
+BIT_SH(bit_rol, brol)
+BIT_SH(bit_ror, bror)
 
-int luaopen_bit_local(lua_State *L) {
-	luaL_register(L, "bit", bitlib);
-	lua_pushnumber(L, BIT_BITS);
-	lua_setfield(L, -2, "bits");
+static int bit_bswap(lua_State *L)
+{
+	UBits b = barg(L, 1);
+	b = (b >> 24) | ((b >> 8) & 0xff00) | ((b & 0xff00) << 8) | (b << 24);
+	BRET(b)
+}
+
+static int bit_tohex(lua_State *L)
+{
+	UBits b = barg(L, 1);
+	SBits n = lua_isnone(L, 2) ? 8 : (SBits)barg(L, 2);
+	const char *hexdigits = "0123456789abcdef";
+	char buf[8];
+	int i;
+	if (n < 0) { n = -n; hexdigits = "0123456789ABCDEF"; }
+	if (n > 8) n = 8;
+	for (i = (int)n; --i >= 0;) { buf[i] = hexdigits[b & 15]; b >>= 4; }
+	lua_pushlstring(L, buf, (size_t)n);
 	return 1;
 }
+
+static const struct luaL_Reg bit_funcs[] = {
+	{ "tobit", bit_tobit },
+	{ "bnot", bit_bnot },
+	{ "band", bit_band },
+	{ "bor", bit_bor },
+	{ "bxor", bit_bxor },
+	{ "lshift", bit_lshift },
+	{ "rshift", bit_rshift },
+	{ "arshift", bit_arshift },
+	{ "rol", bit_rol },
+	{ "ror", bit_ror },
+	{ "bswap", bit_bswap },
+	{ "tohex", bit_tohex },
+	{ NULL, NULL }
+};
+
+/* Signed right-shifts are implementation-defined per C89/C99.
+** But the de facto standard are arithmetic right-shifts on two's
+** complement CPUs. This behavior is required here, so test for it.
+*/
+#define BAD_SAR		(bsar(-8, 2) != (SBits)-2)
+
+int luaopen_bit_local(lua_State *L)
+{
+	UBits b;
+	lua_pushnumber(L, (lua_Number)1437217655L);
+	b = barg(L, -1);
+	if (b != (UBits)1437217655L || BAD_SAR) {  /* Perform a simple self-test. */
+		const char *msg = "compiled with incompatible luaconf.h";
+#ifdef LUA_NUMBER_DOUBLE
+#ifdef _WIN32
+		if (b == (UBits)1610612736L)
+			msg = "use D3DCREATE_FPU_PRESERVE with DirectX";
+#endif
+		if (b == (UBits)1127743488L)
+			msg = "not compiled with SWAPPED_DOUBLE";
+#endif
+		if (BAD_SAR)
+			msg = "arithmetic right-shift broken";
+		luaL_error(L, "bit library self-test failed (%s)", msg);
+	}
+#if LUA_VERSION_NUM < 502
+	luaL_register(L, "bit", bit_funcs);
+#else
+	luaL_newlib(L, bit_funcs);
+#endif
+	return 1;
+}
+
