dde20cca7a0fc95844f34727a9c49ff3f3890ad6
issac <issac.xu@peitian.com>
2017-09-12 10:42:33 +0800

    random access textures array

--- Client/trunk/ParaEngineClient/BlockEngine/BlockTemplate.h
+++ Client/trunk/ParaEngineClient/BlockEngine/BlockTemplate.h
@@ -43,6 +43,8 @@ namespace ParaEngine
 			batt_color_data		= 0x0080000,	// whether the block contains color in its block data.
 
 			batt_invisible = 0x0100000,// whether the block is invisible.
+
+			batt_randomTexture	= 0x0200000,
 		};
 
 		BlockTemplate(uint16_t id,uint32_t attFlag, uint16_t category_id);
