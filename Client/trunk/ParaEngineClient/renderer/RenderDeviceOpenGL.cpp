63d4c1d78c5c7bfb4f9e610ff1aea2c037f42a3b
袁全伟 <573011406@qq.com>
2017-11-10 15:15:28 +0800

    Fix clear bug.

--- Client/trunk/ParaEngineClient/renderer/RenderDeviceOpenGL.cpp
+++ Client/trunk/ParaEngineClient/renderer/RenderDeviceOpenGL.cpp
@@ -323,10 +323,16 @@ HRESULT ParaEngine::RenderDevice::Clear(DWORD Count, const void* pRects, DWORD F
 	}
 	else
 	{
+
+#ifndef PARAENGINE_MOBILE
+		glClear(fields);
+#else
 		// this is done by cocos at the beginning of each tick, we just set default values.
 		// glClear(fields);
 		if (Flags == D3DCLEAR_STENCIL)
 			glClear(GL_STENCIL_BUFFER_BIT);
+#endif
+
 	}
 	return S_OK;
 }
