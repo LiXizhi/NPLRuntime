5b00370b93375ef36d56b3b9c02a854eeb8be689
LiXizhi <lixizhi@yeah.net>
2017-12-05 17:28:26 +0800

    appveyor only deploy on master

--- build_win32.bat
+++ build_win32.bat
@@ -1,13 +1,13 @@
 rem author: lixizhi@yeah.net
 rem date: 2016.2.26
-rem desc: Run this file in developer command prompt x86 (search in desktop search box)
+rem desc: Run this file in developer command prompt x86 visual studio 2017 (search in desktop search box)
 rem Install dependencies: (if DirectX9 SDK is not found, opengl is used)
 rem guide: optionally one can add `BOOST_ROOT` to environment variable, such as 'D:\lxzsrc\NPLRuntime\Server\trunk\boost_1_60_0', 
 rem        make sure to prebuilt your boost library like below
-rem You can choose to build two versions: Client or Server. Only client build requires DirectX SDK installed. When building Client, there is a CMAKE option called ParaEngineClient_DLL to build NPL runtime as dll or executable. 
-rem To build server version: open `NPLRuntime/cmakelist.txt` with cmake-gui or run this bat file from visual c++ env command line.
-rem To build client version: open `Client/cmakelist.txt` with cmake-gui (needs to install DirectX9 SDK in default location and select ParaEngineClient_DLL cmake option)
-  
+rem You can choose to build two versions: Client or Server. Only client build requires DirectX SDK or opengl installed. When building Client, there are CMAKE options to specify whether to build NPL runtime as dll or executable, whether to use static linking in one big executable or using several dlls, etc. 
+rem - To build server version: open `NPLRuntime/cmakelist.txt` with cmake-gui
+rem - To build client version: open `Client/cmakelist.txt` with cmake-gui or simply run this bat file from visual c++ env command line.
+
 pushd .
 
 rem Install cmake
