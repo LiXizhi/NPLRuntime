7c58dc363ac3aa29b935295e265b174f688737f5
LiXizhi <lixizhi@yeah.net>
2017-12-05 15:55:39 +0800

    Fix app veyor build

--- build_win32.bat
+++ build_win32.bat
@@ -12,35 +12,37 @@ pushd .
 
 rem Install cmake
 if NOT EXIST "bin\cmake" (
-	cd bin
+	pushd bin
 	powershell -Command "Invoke-WebRequest https://cmake.org/files/v3.10/cmake-3.10.0-win64-x64.zip -OutFile cmake.zip"
 	7z x cmake.zip -obin > nul
 	move bin\cmake-* cmake
 	cmake\bin\cmake.exe --version
-	cd ..
+	popd
 )
 
 rem Install Boost
 if NOT EXIST "bin\boost" (
-	cd bin
-	powershell -Command "Invoke-WebRequest http://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.zip -OutFile boost.zip"
-	7z x boost.zip -obin > nul
+	pushd bin
+	powershell -Command "Invoke-WebRequest http://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.7z -OutFile boost.7z"
+	7z x boost.7z -obin > nul
 	move bin\boost_* boost
 	cd boost
 	bootstrap.bat
 	b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-signals --with-serialization --with-iostreams --with-regex stage
-	cd ..\..
+	popd
 )
-set BOOST_ROOT="%~dp0\boost"
+set BOOST_ROOT=%~dp0bin\boost
 
 rem Build main executable
 mkdir bin\client_win32
 cd bin\client_win32
 
-rem call "..\cmake\bin\cmake.exe" ../../Client/
-rem msbuild  %~dp0\bin\client_win32\CLIENT.sln /verbosity:minimal /property:Configuration=Release
+call "..\cmake\bin\cmake.exe" ../../Client/
+msbuild  %~dp0\bin\client_win32\CLIENT.sln /verbosity:minimal /property:Configuration=Release
+
+popd
 
 dir ParaWorld/bin32
 
-popd
+
 
