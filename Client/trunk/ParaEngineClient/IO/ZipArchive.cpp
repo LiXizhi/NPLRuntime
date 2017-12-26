b63235bafa4d34e10ce44070579ae93ba41bd620
kkvskkkk <460864076@qq.com>
2017-12-26 17:49:45 +0800

    * optimization ZipArchive

--- Client/trunk/ParaEngineClient/IO/ZipArchive.cpp
+++ Client/trunk/ParaEngineClient/IO/ZipArchive.cpp
@@ -320,7 +320,7 @@ bool CZipArchive::OpenZipFile(const string& filename)
 		//PERF_END(sArchiveName.c_str());
 
 		// prepare file index for binary search
-		Sort();
+		//Sort();
 
 		if(m_bOpened)
 		{
@@ -387,6 +387,8 @@ bool CZipArchive::GeneratePkgFile( const char* filename )
 		return false;
 
 	ParaEngine::Lock lock_(m_mutex);
+
+	Sort();
 
 	// pkg header
 	file.WriteString(".pkg",4);
