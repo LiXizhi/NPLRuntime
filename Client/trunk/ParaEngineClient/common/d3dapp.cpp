a3bf79108a71b37347f0f299834018c24770f35f
袁全伟 <573011406@qq.com>
2018-01-06 19:54:52 +0800

    Clear Initialize3DEnvironment

--- Client/trunk/ParaEngineClient/common/d3dapp.cpp
+++ Client/trunk/ParaEngineClient/common/d3dapp.cpp
@@ -906,23 +906,7 @@ HRESULT CD3DApplication::Initialize3DEnvironment()
     if( m_bCreateMultithreadDevice )
         behaviorFlags |= D3DCREATE_MULTITHREADED;
 
-    // Create the device
-	/* D3DCREATE_FPU_PRESERVE:
-	There's also an issue about Direct X autonomously changing the internal FPU accuracy, 
-	leading to inconsistent calculations. Direct3D by default changes the FPU to single precision 
-	mode at program startup and does not change it back to improve speed 
-	(This means that your double variables will behave like float). 
-	To disable this behaviour, pass the D3DCREATE_FPU_PRESERVE flag to the CreateDevice call. 
-	This might adversely affect D3D performance (not much though).
-	
-	D3DCREATE_NOWINDOWCHANGES :
-	Let ParaEngine to manage window focus changes, such as Alt-tab and mouse activate. 
-	*/
 
-	
-    //hr = m_pD3D->CreateDevice( m_d3dSettings.AdapterOrdinal(), pDeviceInfo->DevType,
-    //                           m_hWndFocus, behaviorFlags | D3DCREATE_FPU_PRESERVE /*| D3DCREATE_NOWINDOWCHANGES*/, &m_d3dpp,
-    //                           &m_pd3dDevice );
 	RenderDeviceConfiguration cfg;
 	cfg.isWindowed = !m_bStartFullscreen;
 	cfg.renderWindow = m_pRenderWindow;
@@ -939,232 +923,38 @@ HRESULT CD3DApplication::Initialize3DEnvironment()
 
 	m_pRenderDevice = new CD3D9RenderDevice(m_pd3dDevice);
 	CGlobals::SetRenderDevice(m_pRenderDevice);
-	//following code create nvidia perfhud device for performance testing --clayman
-	/*
-	UINT AdapterToUse=D3DADAPTER_DEFAULT;
-	for (UINT Adapter=0;Adapter<m_pD3D->GetAdapterCount();Adapter++)
+
+
+	// Store render target surface desc
+	LPDIRECT3DSURFACE9 pBackBuffer = NULL;
+	m_pd3dDevice->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &pBackBuffer);
+	pBackBuffer->GetDesc(&m_d3dsdBackBuffer);
+	pBackBuffer->Release();
+
+	// Initialize the app's device-dependent objects
+	hr = InitDeviceObjects();
+
+
+
+	if (FAILED(hr))
 	{
-		D3DADAPTER_IDENTIFIER9 Identifier;
-		HRESULT Res;
-		Res = m_pD3D->GetAdapterIdentifier(Adapter,0,&Identifier);
-		if (strstr(Identifier.Description,"PerfHUD") != 0)
-		{
-			AdapterToUse=Adapter;
-			break;
-		}
-		}
-
-	hr = m_pD3D->CreateDevice( AdapterToUse, D3DDEVTYPE_REF,
-		m_hWndFocus, D3DCREATE_HARDWARE_VERTEXPROCESSING , &m_d3dpp,
-		&m_pd3dDevice );
-	*/
-
-	if(FAILED(hr))
+		DeleteDeviceObjects();
+	}
+	else
 	{
-		OUTPUT_LOG("error: Can not create d3d device with the said settings: %d*%d\n", m_d3dpp.BackBufferWidth, m_d3dpp.BackBufferHeight);
-		InterpretError(hr, __FILE__, __LINE__);
-		// for web browser, sometimes it is not possible to create device due to unknown reason.  
-		for (int i=0;i<3;i++)
+		m_bDeviceObjectsInited = true;
+		hr = RestoreDeviceObjects();
+		if (FAILED(hr))
 		{
-			::Sleep(1000);
-			
-			GetClientRect( m_hWnd, &m_rcWindowClient );
-			m_nClientWidth = m_rcWindowClient.right - m_rcWindowClient.left;
-			m_nClientHeight = m_rcWindowClient.bottom - m_rcWindowClient.top;
-			m_d3dSettings.Windowed_Width = m_nClientWidth;
-			m_d3dSettings.Windowed_Height = m_nClientHeight;
-
-			m_d3dpp.BackBufferWidth  = m_d3dSettings.Windowed_Width;
-			m_d3dpp.BackBufferHeight = m_d3dSettings.Windowed_Height;
-
-			// some graphics card's multi-sample  quality will return 1 in CheckDeviceMultiSampleType, but actually fail when creating device. 
-			// this will somehow fix the problem. 
-			/*m_d3dSettings.Fullscreen_MultisampleType = D3DMULTISAMPLE_NONE;
-			m_d3dSettings.Fullscreen_MultisampleQuality = 0;
-			m_d3dSettings.Windowed_MultisampleQuality = D3DMULTISAMPLE_NONE;
-			m_d3dSettings.Windowed_MultisampleQuality = 0;*/
-			m_d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE;
-			m_d3dpp.MultiSampleQuality = 0;
-			
-
-			OUTPUT_LOG("trying recreating 3d device with %d*%d\n", m_d3dpp.BackBufferWidth, m_d3dpp.BackBufferHeight);
-			if(m_d3dpp.BackBufferWidth < 1)
-				m_d3dpp.BackBufferWidth = 960;
-			if(m_d3dpp.BackBufferHeight < 1)
-				m_d3dpp.BackBufferHeight = 560;
-
-			hr = m_pD3D->CreateDevice( m_d3dSettings.AdapterOrdinal(), pDeviceInfo->DevType,
-				m_hWndFocus, behaviorFlags | D3DCREATE_FPU_PRESERVE, &m_d3dpp,
-				&m_pd3dDevice );
-			if( SUCCEEDED(hr) )
-			{
-				OUTPUT_LOG("Successfully created 3d device\n");
-				break;
-			}
+			InvalidateDeviceObjects();
+		}
+		else
+		{
+			m_bDeviceObjectsRestored = true;
+			return S_OK;
 		}
 	}
 
-    if( SUCCEEDED(hr) )
-    {
-        // When moving from fullscreen to windowed mode, it is important to
-        // adjust the window size after recreating the device rather than
-        // beforehand to ensure that you get the window size you want.  For
-        // example, when switching from 640x480 fullscreen to windowed with
-        // a 1000x600 window on a 1024x768 desktop, it is impossible to set
-        // the window size to 1000x600 until after the display mode has
-        // changed to 1024x768, because windows cannot be larger than the
-        // desktop.
-        if( m_bWindowed && !m_bIsExternalWindow)
-        {
-			// NOTE: this could be wrong if the window is a child window. For child window, always specify m_bIsExternalWindow to true.
-            SetWindowPos( m_hWnd, HWND_NOTOPMOST,
-                          m_rcWindowBounds.left, m_rcWindowBounds.top,
-                          ( m_rcWindowBounds.right - m_rcWindowBounds.left ),
-                          ( m_rcWindowBounds.bottom - m_rcWindowBounds.top ),
-						  SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOZORDER);
-        }
-
-        // Store device Caps
-        m_pd3dDevice->GetDeviceCaps( &m_d3dCaps );
-        m_dwCreateFlags = behaviorFlags;
-
-        // Store device description
-        if( pDeviceInfo->DevType == D3DDEVTYPE_REF )
-            lstrcpy( m_strDeviceStats, TEXT("REF") );
-        else if( pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-            lstrcpy( m_strDeviceStats, TEXT("HAL") );
-        else if( pDeviceInfo->DevType == D3DDEVTYPE_SW )
-            lstrcpy( m_strDeviceStats, TEXT("SW") );
-
-        if( behaviorFlags & D3DCREATE_HARDWARE_VERTEXPROCESSING &&
-            behaviorFlags & D3DCREATE_PUREDEVICE )
-        {
-            if( pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-                lstrcat( m_strDeviceStats, TEXT(" (pure hw vp)") );
-            else
-                lstrcat( m_strDeviceStats, TEXT(" (simulated pure hw vp)") );
-        }
-        else if( behaviorFlags & D3DCREATE_HARDWARE_VERTEXPROCESSING )
-        {
-            if( pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-                lstrcat( m_strDeviceStats, TEXT(" (hw vp)") );
-            else
-                lstrcat( m_strDeviceStats, TEXT(" (simulated hw vp)") );
-        }
-        else if( behaviorFlags & D3DCREATE_MIXED_VERTEXPROCESSING )
-        {
-            if( pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-                lstrcat( m_strDeviceStats, TEXT(" (mixed vp)") );
-            else
-                lstrcat( m_strDeviceStats, TEXT(" (simulated mixed vp)") );
-        }
-        else if( behaviorFlags & D3DCREATE_SOFTWARE_VERTEXPROCESSING )
-        {
-            lstrcat( m_strDeviceStats, TEXT(" (sw vp)") );
-        }
-
-        if( pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-        {
-            // Be sure not to overflow m_strDeviceStats when appending the adapter 
-            // description, since it can be long.  Note that the adapter description
-            // is initially CHAR and must be converted to TCHAR.
-            lstrcat( m_strDeviceStats, TEXT(": ") );
-            const int cchDesc = sizeof(pAdapterInfo->AdapterIdentifier.Description);
-            TCHAR szDescription[cchDesc];
-            DXUtil_ConvertAnsiStringToGenericCch( szDescription, 
-                pAdapterInfo->AdapterIdentifier.Description, cchDesc );
-            int maxAppend = sizeof(m_strDeviceStats) / sizeof(TCHAR) -
-                lstrlen( m_strDeviceStats ) - 1;
-            _tcsncat( m_strDeviceStats, szDescription, maxAppend );
-        }
-
-        // Store render target surface desc
-        LPDIRECT3DSURFACE9 pBackBuffer = NULL;
-        m_pd3dDevice->GetBackBuffer( 0, 0, D3DBACKBUFFER_TYPE_MONO, &pBackBuffer );
-        pBackBuffer->GetDesc( &m_d3dsdBackBuffer );
-        pBackBuffer->Release();
-
-        // Set up the fullscreen cursor
-        if(  !m_bWindowed )
-        {
-//            HCURSOR hCursor;
-//#ifdef _WIN64
-//            hCursor = (HCURSOR)GetClassLongPtr( m_hWnd, GCLP_HCURSOR );
-//#else
-//            hCursor = (HCURSOR)ULongToHandle( GetClassLong( m_hWnd, GCL_HCURSOR ) );
-//#endif
-//            D3DUtil_SetDeviceCursor( m_pd3dDevice, hCursor, true );
-//            m_pd3dDevice->ShowCursor( true );
-			
-        }
-
-        // Confine cursor to fullscreen window
-        if( m_bClipCursorWhenFullscreen )
-        {
-            if (!m_bWindowed )
-            {
-                RECT rcWindow;
-                GetWindowRect( m_hWnd, &rcWindow );
-                ClipCursor( &rcWindow );
-            }
-            else
-            {
-                ClipCursor( NULL );
-            }
-        }
-        // Initialize the app's device-dependent objects
-        hr = InitDeviceObjects();
-
-        if( FAILED(hr) )
-        {
-            DeleteDeviceObjects();
-        }
-        else
-        {
-            m_bDeviceObjectsInited = true;
-            hr = RestoreDeviceObjects();
-            if( FAILED(hr) )
-            {
-                InvalidateDeviceObjects();
-            }
-            else
-            {
-                m_bDeviceObjectsRestored = true;
-                return S_OK;
-            }
-        }
-
-        // Cleanup before we try again
-        Cleanup3DEnvironment();
-    }
-
-    // If that failed, fall back to the reference rasterizer
-    if( hr != D3DAPPERR_MEDIANOTFOUND && 
-        hr != HRESULT_FROM_WIN32( ERROR_FILE_NOT_FOUND ) && 
-        pDeviceInfo->DevType == D3DDEVTYPE_HAL )
-    {
-		OUTPUT_LOG("warning: Can not create d3d device with the said settings: %d*%d\n", m_d3dpp.BackBufferWidth, m_d3dpp.BackBufferHeight);
-		InterpretError(hr, __FILE__, __LINE__);
-
-        if (FindBestWindowedMode(false, true))
-        {
-            m_bWindowed = true;
-			if(!m_bIsExternalWindow)
-			{
-				AdjustWindowForChange();
-				// Make sure main window isn't topmost, so error message is visible
-				SetWindowPos( m_hWnd, HWND_NOTOPMOST,
-							  m_rcWindowBounds.left, m_rcWindowBounds.top,
-							  ( m_rcWindowBounds.right - m_rcWindowBounds.left ),
-							  ( m_rcWindowBounds.bottom - m_rcWindowBounds.top ),
-							  SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOZORDER);
-			}
-            // Let the user know we are switching from HAL to the reference rasterizer
-            DisplayErrorMsg( hr, MSGWARN_SWITCHEDTOREF );
-
-			hr = Initialize3DEnvironment();
-        }
-    }
     return hr;
 }
 
