ada1e850b28fca70e819ef99abcb49eabdcdd20a
袁全伟 <573011406@qq.com>
2018-01-28 10:58:23 +0800

    EVirtualKey

--- Client/trunk/ParaEngineClient/ParaEngine/3dengine/AutoCamera.cpp
+++ Client/trunk/ParaEngineClient/ParaEngine/3dengine/AutoCamera.cpp
@@ -1904,20 +1904,20 @@ void CAutoCamera::HandleUserInput()
 	bool bAlterKeyPressed = false;
 	if (pKeyboard)
 	{
-		bAlterKeyPressed = pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT_CONTROL) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT_CONTROL) ||
+		bAlterKeyPressed = pKeyboard->IsKeyPressed(EVirtualKey::KEY_LCONTROL) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RCONTROL) ||
 			// we still needs shift toggle run/walk movement
 			// pKeyboard->IsKeyPressed(DIK_LSHIFT) || pKeyboard->IsKeyPressed(DIK_RSHIFT) ||
-			pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT_ALT) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT_ALT);
+			pKeyboard->IsKeyPressed(EVirtualKey::KEY_LMENU) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RMENU);
 		bIsKeyProcessed = bIsKeyProcessed || bAlterKeyPressed;
 	}
 
-	SetKeyDownState(MOVE_FORWARD, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_FORWARD)/*DIK_W*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_8) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_UP)));
-	SetKeyDownState(MOVE_BACKWARD, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_BACKWARD)/*DIK_S*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_2) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_DOWN)));
+	SetKeyDownState(MOVE_FORWARD, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_FORWARD)/*DIK_W*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD8) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_UP)));
+	SetKeyDownState(MOVE_BACKWARD, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_BACKWARD)/*DIK_S*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD2) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_DOWN)));
 	
 	if(IsShiftMoveSwitched())
 	{
-		SetKeyDownState(SHIFT_LEFT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(SHIFT_LEFT)) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_7) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT)));
-		SetKeyDownState(SHIFT_RIGHT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(SHIFT_RIGHT)) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_9) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT)));
+		SetKeyDownState(SHIFT_LEFT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(SHIFT_LEFT)) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD7) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT)));
+		SetKeyDownState(SHIFT_RIGHT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(SHIFT_RIGHT)) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD9) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT)));
 		SetKeyDownState(MOVE_LEFT, FALSE);
 		SetKeyDownState(MOVE_RIGHT, FALSE);
 	}
@@ -1925,8 +1925,8 @@ void CAutoCamera::HandleUserInput()
 	{
 		SetKeyDownState(SHIFT_LEFT, FALSE);
 		SetKeyDownState(SHIFT_RIGHT, FALSE);
-		SetKeyDownState(MOVE_LEFT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_LEFT)/*DIK_A*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_4) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT)));
-		SetKeyDownState(MOVE_RIGHT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_RIGHT)/*DIK_D*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD_6) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT)));
+		SetKeyDownState(MOVE_LEFT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_LEFT)/*DIK_A*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD4) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_LEFT)));
+		SetKeyDownState(MOVE_RIGHT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(MOVE_RIGHT)/*DIK_D*/) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_NUMPAD6) || pKeyboard->IsKeyPressed(EVirtualKey::KEY_RIGHT)));
 	}
 	SetKeyDownState(ZOOM_IN, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(ZOOM_IN)/*DIK_INSERT*/)));
 	SetKeyDownState(ZOOM_OUT, !bIsKeyProcessed && (pKeyboard->IsKeyPressed(GetKeyMap(ZOOM_OUT)/*DIK_DELETE*/)));
--- Client/trunk/ParaEngineClient/ParaEngine/Framework/InputSystem/VirtualKey.h
+++ Client/trunk/ParaEngineClient/ParaEngine/Framework/InputSystem/VirtualKey.h
@@ -18,112 +18,151 @@ namespace ParaEngine
 
 	enum class EVirtualKey
 	{
-		KEY_UNKNOWN = -1,
-		/// Printable keys
-		KEY_SPACE,
-		KEY_APOSTROPHE,		// '
-		KEY_COMMA,			// ,
-		KEY_MINUS,			// -
-		KEY_PERIOD,			// .
-		KEY_SLASH,			// /
-		KEY_0,
-		KEY_1,
-		KEY_2,
-		KEY_3,
-		KEY_4,
-		KEY_5,
-		KEY_6,
-		KEY_7,
-		KEY_8,
-		KEY_9,
-		KEY_SEMICOLON,		// ;
-		KEY_EQUAL,			// =
-		KEY_A,
-		KEY_B,
-		KEY_C,
-		KEY_D,
-		KEY_E,
-		KEY_F,
-		KEY_G,
-		KEY_H,
-		KEY_I,
-		KEY_J,
-		KEY_K,
-		KEY_L,
-		KEY_M,
-		KEY_N,
-		KEY_O,
-		KEY_P,
-		KEY_Q,
-		KEY_R,
-		KEY_S,
-		KEY_T,
-		KEY_U,
-		KEY_V,
-		KEY_W,
-		KEY_X,
-		KEY_Y,
-		KEY_Z,
-		KEY_LEFT_BRACKET,		/** [ **/
-		KEY_BACKSLASH,			/** \ **/
-		KEY_RIGHT_BRACKET,		/** ]  **/
-		KEY_GRAVE_ACCENT,		/** ` **/
-
-		/// Function Keys
-		KEY_ESCAPE,
-		KEY_ENTER,
-		KEY_TAB,
-		KEY_BACKSPACE,
-		KEY_INSERT,
-		KEY_DELETE,
-		KEY_RIGHT,
-		KEY_LEFT,
-		KEY_DOWN,
-		KEY_UP,
-		KEY_PAGE_UP,
-		KEY_PAGE_DOWN,
-		KEY_HOME,
-		KEY_END,
-		KEY_CAPS_LOCK,
-		KEY_SCROLL_LOCK,
-		KEY_NUM_LOCK,
-		KEY_PRINT_SCREEN,
-		KEY_PAUSE,
-		KEY_F1,
-		KEY_F2,
-		KEY_F3,
-		KEY_F4,
-		KEY_F5,
-		KEY_F6,
-		KEY_F7,
-		KEY_F8,
-		KEY_F9,
-		KEY_F10,
-		KEY_F11,
-		KEY_F12,
-		KEY_NUMPAD_0,
-		KEY_NUMPAD_1,
-		KEY_NUMPAD_2,
-		KEY_NUMPAD_3,
-		KEY_NUMPAD_4,
-		KEY_NUMPAD_5,
-		KEY_NUMPAD_6,
-		KEY_NUMPAD_7,
-		KEY_NUMPAD_8,
-		KEY_NUMPAD_9,
-		KEY_NUMPAD_DECIMAL, /** . **/
-		KEY_NUMPAD_DIVIDE,
-		KEY_NUMPAD_MULTIPLY,
-		KEY_NUMPAD_SUBTRACT,
-		KEY_NUMPAD_ADD,
-		KEY_NUMPAD_ENTER,
-		KEY_NUMPAD_EQUAL,
-		KEY_LEFT_SHIFT,
-		KEY_LEFT_CONTROL,
-		KEY_LEFT_ALT,
-		KEY_RIGHT_SHIFT,
-		KEY_RIGHT_CONTROL,
-		KEY_RIGHT_ALT,
-		COUNT,
+		KEY_UNKNOWN = 0,
+		KEY_ESCAPE = 1,
+		KEY_1 = 2,
+		KEY_2 = 3,
+		KEY_3 = 4,
+		KEY_4 = 5,
+		KEY_5 = 6,
+		KEY_6 = 7,
+		KEY_7 = 8,
+		KEY_8 = 9,
+		KEY_9 = 10,
+		KEY_0 = 11,
+		KEY_MINUS = 12,     /* - on main keyboard */
+		KEY_EQUALS = 13,
+		KEY_BACK = 14,     /* backspace */
+		KEY_TAB = 15,
+		KEY_Q = 16,
+		KEY_W = 17,
+		KEY_E = 18,
+		KEY_R = 19,
+		KEY_T = 20,
+		KEY_Y = 21,
+		KEY_U = 22,
+		KEY_I = 23,
+		KEY_O = 24,
+		KEY_P = 25,
+		KEY_LBRACKET = 26,
+		KEY_RBRACKET = 27,
+		KEY_RETURN = 28,    /* Enter on main keyboard */
+		KEY_LCONTROL = 29,
+		KEY_A = 30,
+		KEY_S = 31,
+		KEY_D = 32,
+		KEY_F = 33,
+		KEY_G = 34,
+		KEY_H = 35,
+		KEY_J = 36,
+		KEY_K = 37,
+		KEY_L = 38,
+		KEY_SEMICOLON = 39,
+		KEY_APOSTROPHE = 40,
+		KEY_GRAVE = 41,    /* accent grave */
+		KEY_LSHIFT = 42,
+		KEY_BACKSLASH = 43,
+		KEY_Z = 44,
+		KEY_X = 45,
+		KEY_C = 46,
+		KEY_V = 47,
+		KEY_B = 48,
+		KEY_N = 49,
+		KEY_M = 50,
+		KEY_COMMA = 51,
+		KEY_PERIOD = 52,  /* . on main keyboard */
+		KEY_SLASH = 53,  /* / on main keyboard */
+		KEY_RSHIFT = 54,
+		KEY_MULTIPLY = 55,  /* * on numeric keypad */
+		KEY_LMENU = 56,  /* left Alt */
+		KEY_SPACE = 57,
+		KEY_CAPITAL = 58,
+		KEY_F1 = 59,
+		KEY_F2 = 60,
+		KEY_F3 = 61,
+		KEY_F4 = 62,
+		KEY_F5 = 63,
+		KEY_F6 = 64,
+		KEY_F7 = 65,
+		KEY_F8 = 66,
+		KEY_F9 = 67,
+		KEY_F10 = 68,
+		KEY_NUMLOCK = 69,
+		KEY_SCROLL = 70,  /* Scroll Lock */
+		KEY_NUMPAD7 = 71,
+		KEY_NUMPAD8 = 72,
+		KEY_NUMPAD9 = 73,
+		KEY_SUBTRACT = 74,  /* - on numeric keypad */
+		KEY_NUMPAD4 = 75,
+		KEY_NUMPAD5 = 76,
+		KEY_NUMPAD6 = 77,
+		KEY_ADD = 78,  /* + on numeric keypad */
+		KEY_NUMPAD1 = 79,
+		KEY_NUMPAD2 = 80,
+		KEY_NUMPAD3 = 81,
+		KEY_NUMPAD0 = 82,
+		KEY_DECIMAL = 83,  /* . on numeric keypad */
+		KEY_OEM_102 = 86,  /* <> or \| on RT 102-key keyboard (Non-U.S.) */
+		KEY_F11 = 87,
+		KEY_F12 = 88,
+		KEY_F13 = 100,  /*                     (NEC PC98) */
+		KEY_F14 = 101,  /*                     (NEC PC98) */
+		KEY_F15 = 102,  /*                     (NEC PC98) */
+		KEY_KANA = 112,  /* (Japanese keyboard)            */
+		KEY_ABNT_C1 = 115,  /* /? on Brazilian keyboard */
+		KEY_CONVERT = 121,  /* (Japanese keyboard)            */
+		KEY_NOCONVERT = 123,  /* (Japanese keyboard)            */
+		KEY_YEN = 125,  /* (Japanese keyboard)            */
+		KEY_ABNT_C2 = 126,  /* Numpad . on Brazilian keyboard */
+		KEY_NUMPADEQUALS = 141,  /* =tonumber(" on numeric keypad (NEC PC98) */
+		KEY_PREVTRACK = 144,  /* Previous Track (KEY_CIRCUMFLEX on Japanese keyboard) */
+		KEY_AT = 145,  /*                     (NEC PC98) */
+		KEY_COLON = 146,  /*                     (NEC PC98) */
+		KEY_UNDERLINE = 147,  /*                     (NEC PC98) */
+		KEY_KANJI = 148,  /* (Japanese keyboard)            */
+		KEY_STOP = 149,  /*                     (NEC PC98) */
+		KEY_AX = 150,  /*                     (Japan AX) */
+		KEY_UNLABELED = 151,  /*                        (J3100) */
+		KEY_NEXTTRACK = 153,  /* Next Track */
+		KEY_NUMPADENTER = 156,  /* Enter on numeric keypad */
+		KEY_RCONTROL = 157,
+		KEY_MUTE = 160,  /* Mute */
+		KEY_CALCULATOR = 161,  /* Calculator */
+		KEY_PLAYPAUSE = 162,  /* Play / Pause */
+		KEY_MEDIASTOP = 164,  /* Media Stop */
+		KEY_VOLUMEDOWN = 174,  /* Volume - */
+		KEY_VOLUMEUP = 176,  /* Volume + */
+		KEY_WEBHOME = 178,  /* Web home */
+		KEY_NUMPADCOMMA = 179,  /* ",16), on numeric keypad (NEC PC98) */
+		KEY_DIVIDE = 181,  /* / on numeric keypad */
+		KEY_SYSRQ = 183,
+		KEY_RMENU = 184,  /* right Alt */
+		KEY_PAUSE = 197,  /* Pause */
+		KEY_HOME = 199,  /* Home on arrow keypad */
+		KEY_UP = 200,  /* UpArrow on arrow keypad */
+		KEY_PRIOR = 201,  /* PgUp on arrow keypad */
+		KEY_LEFT = 203,  /* LeftArrow on arrow keypad */
+		KEY_RIGHT = 205,  /* RightArrow on arrow keypad */
+		KEY_END = 207,  /* End on arrow keypad */
+		KEY_DOWN = 208,  /* DownArrow on arrow keypad */
+		KEY_NEXT = 209,  /* PgDn on arrow keypad */
+		KEY_INSERT = 210,  /* Insert on arrow keypad */
+		KEY_DELETE = 211,  /* Delete on arrow keypad */
+		KEY_LWIN = 219,  /* Left Windows key */
+		KEY_RWIN = 220,  /* Right Windows key */
+		KEY_APPS = 221,  /* AppMenu key */
+		KEY_POWER = 222,  /* System Power */
+		KEY_SLEEP = 223,  /* System Sleep */
+		KEY_WAKE = 227,  /* System Wake */
+		KEY_WEBSEARCH = 229,  /* Web Search */
+		KEY_WEBFAVORITES = 230,  /* Web Favorites */
+		KEY_WEBREFRESH = 231,  /* Web Refresh */
+		KEY_WEBSTOP = 232,  /* Web Stop */
+		KEY_WEBFORWARD = 233,  /* Web Forward */
+		KEY_WEBBACK = 234,  /* Web Back */
+		KEY_MYCOMPUTER = 235,  /* My Computer */
+		KEY_MAIL = 236,  /* Mail */
+		KEY_MEDIASELECT = 237,  /* Media Select */
+		COUNT = 238,
 	};
 }
