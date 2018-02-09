4f7deb477473777575476ec140f6d900e3fbfbc7
袁全伟 <qq573011406@126.com>
2018-02-09 18:12:10 +0800

    android tegra

---
+++ Client/trunk/ParaEngineClient/Platform/Android/AndroidApplication.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <android_native_app_glue.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+namespace ParaEngine
+{
+    class AndroidApplication 
+    {
+    public:
+        AndroidApplication(struct android_app* app);
+        ~AndroidApplication();
+        void Run();
+    private:
+        struct android_app* m_App;
+        EGLDisplay m_Display;
+        EGLSurface m_Surface;
+        EGLContext m_Context;
+        int32_t m_Width;
+        int32_t m_Height;
+
+        static void app_handle_command(struct android_app* app, int32_t cmd);
+        static int32_t app_handle_input(struct android_app* app, AInputEvent* event);
+    protected:
+        // App commands
+        virtual void OnStart();
+        virtual void OnStop();
+        virtual void OnPause();
+        virtual void OnResume();
+        virtual void OnDestroy();
+        virtual void OnInitWindow();
+        virtual void OnTermWindow();
+        virtual void OnWindowResized();
+    };
+}
