40fe828f50f509ee1254c8b56065463ca41f397f
袁全伟 <573011406@qq.com>
2018-02-18 13:26:28 +0800

    boost 1.65.1 for anroid

--- build_android_tegra.bat
+++ build_android_tegra.bat
@@ -3,5 +3,5 @@ rem Build android
 mkdir build\android_tegra\
 cd build\android_tegra
 
-call "cmake.exe" -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=Android ../../Client/trunk
+call "cmake.exe" -G "Visual Studio 14 2015" -DCMAKE_SYSTEM_NAME=Android ../../NPLRuntime
 popd
