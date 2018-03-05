950c5e8d2dd6a67fc8b4344d8133fb9fb07a9fc9
袁全伟 <qq573011406@126.com>
2018-03-05 13:21:03 +0800

    luajit2.1

---
+++ NPLRuntime/externals/LuaJIT2_1/build_win32.bat
@@ -0,0 +1,30 @@
+@echo off
+
+if "%WindowsSdkDir%" == "" (
+    echo Start this batch script from Visual Studio Command Prompt
+    goto :EOF
+)
+
+set DIR=%~dp0
+set SRCDIR=%DIR%src
+set DESTDIR=%DIR%prebuilt\win32
+
+if exist %SRCDIR%\src\*.lib (
+del %DESTDIR%\*.lib
+)
+if exist %SRCDIR%\src\*.dll (
+del %DESTDIR%\*.dll
+)
+
+cd /d %SRCDIR%\src
+call msvcbuild.bat
+
+if exist %SRCDIR%\src\lua51.lib (
+    copy %SRCDIR%\src\lua51.lib %DESTDIR%
+)
+
+if exist %SRCDIR%\src\lua51.dll (
+    copy %SRCDIR%\src\lua51.dll %DESTDIR%
+)
+
+cd /d %DIR%
