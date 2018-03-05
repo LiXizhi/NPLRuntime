41c9a498b1833ff5e6be519922118e49a6d79c1c
袁全伟 <qq573011406@126.com>
2018-03-05 15:37:51 +0800

    luajit2.1 predefine

--- NPLRuntime/externals/LuaJIT2_1/include/lauxlib.h
+++ NPLRuntime/externals/LuaJIT2_1/include/lauxlib.h
@@ -158,4 +158,26 @@ LUALIB_API void (luaL_pushresult) (luaL_Buffer *B);
 
 /* }====================================================== */
 
+
+/* compatibility with ref system */
+
+/* pre-defined references */
+#define LUA_NOREF       (-2)
+#define LUA_REFNIL      (-1)
+
+#define lua_ref(L,lock) ((lock) ? luaL_ref(L, LUA_REGISTRYINDEX) : \
+      (lua_pushstring(L, "unlocked references are obsolete"), lua_error(L), 0))
+
+#define lua_unref(L,ref)        luaL_unref(L, LUA_REGISTRYINDEX, (ref))
+
+#define lua_getref(L,ref)       lua_rawgeti(L, LUA_REGISTRYINDEX, (ref))
+
+
+#define luaL_reg	luaL_Reg
+
+
 #endif
+
+
+
+
