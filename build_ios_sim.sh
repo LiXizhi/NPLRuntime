71f8f96f336af97f48cbe18d14902ad1da84ea17
hisky <573011406@qq.com>
2018-04-07 08:28:18 +0800

    boost for ios

--- build_ios_sim.sh
+++ build_ios_sim.sh
@@ -1,5 +1,5 @@
 pushd .
 mkdir -p ./build/ios_sim/
 cd build/ios_sim
-cmake ../../NPLRuntime -DCMAKE_TOOLCHAIN_FILE=../../NPLRuntime/cmake/ios.toolchain.cmake -DIOS_PLATFORM=SIMULATOR
+cmake ../../NPLRuntime -G "Xcode" -DCMAKE_TOOLCHAIN_FILE=../../NPLRuntime/cmake/ios.toolchain.cmake -DIOS_PLATFORM=SIMULATOR -DENABLE_BITCODE=OFF
 popd
