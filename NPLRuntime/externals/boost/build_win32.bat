4bdd95adf7ce2d97840b0ed094fec8112288169d
袁全伟 <qq573011406@126.com>
2018-04-26 20:34:11 +0800

    boost

--- NPLRuntime/externals/boost/build_win32.bat
+++ NPLRuntime/externals/boost/build_win32.bat
@@ -17,8 +17,11 @@ rem clean
 echo Cleaning everything
 rem build
 echo Building for win32
+IF NOT EXIST b2.exe (  
 call bootstrap.bat
-b2 stage link=static runtime-link=static threading=multi cflags=-D_WIN32_WINNT=0x0501 --stagedir=build_win32/stage --with-libraries="thread date_time filesystem system chrono signals regex serialization iostreams log"
+)
+
+b2 stage link=static runtime-link=static threading=multi cflags=-D_WIN32_WINNT=0x0501 --stagedir=build_win32/stage --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-signals --with-regex --with-serialization --with-iostreams --with-log
 popd
 rem done
 echo =====================
