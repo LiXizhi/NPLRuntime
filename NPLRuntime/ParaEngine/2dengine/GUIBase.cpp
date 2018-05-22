b6251ab5fbaf8ebf5874c5af60b4144565addfab
LiXizhi <lixizhi@yeah.net>
2018-05-22 18:46:18 +0800

    fix headon GUI display with small viewport

--- NPLRuntime/ParaEngine/2dengine/GUIBase.cpp
+++ NPLRuntime/ParaEngine/2dengine/GUIBase.cpp
@@ -36,6 +36,7 @@
 #include "RenderTarget.h"
 #include "EffectManager.h"
 #include "GUIIME.h"
+#include "ViewportManager.h"
 #include "memdebug.h"
 
 #if USE_DIRECTX_RENDERER
@@ -496,17 +497,37 @@ void CGUIBase::GetAbsolutePosition(CGUIPosition* pOut, const CGUIPosition* pIn)
 	else if(pIn->IsRelativeTo3DObject())
 	{
 		//calculate 3d coordinates of the 3d object
-		Vector3 vIn,vOut;
+		Vector3 vIn, vOut;
 		ParaViewport  viewport;
-		//pIn->Relative.To3D.p3DSceneObject->GetRenderOffset();
-		// vIn.y+=pIn->Relative.To3D.p3DSceneObject->GetHeight();
 		vIn = *(Vector3*)(&(pIn->Relative.To3D.m_v3DPosition));
 
-		auto vp = CGlobals::GetRenderDevice()->GetViewport();
-		viewport.X = vp.x;
-		viewport.Y = vp.y;
-		viewport.Width = vp.z;
-		viewport.Height = vp.w;
+		CViewport* pViewport = CGlobals::GetViewportManager()->GetActiveViewPort();
+
+		// find the closet scene viewport whose index is bigger than the current viewport
+		if (pViewport->GetScene() == 0)
+		{
+			int nCount = CGlobals::GetViewportManager()->GetViewportCount();
+			int nCurIndex = -1;
+			for (int i = 0; i < nCount; ++i)
+			{
+				CViewport* pViewport2 = CGlobals::GetViewportManager()->CreateGetViewPort(i);
+				if (pViewport2->GetScene())
+				{
+					pViewport = pViewport2;
+					if (nCurIndex >= 0)
+						break;
+				}
+				else if (pViewport == pViewport2)
+				{
+					nCurIndex = i;
+				}
+			}
+		}
+		viewport.X = pViewport->GetLeft();
+		viewport.Y = pViewport->GetTop();
+		viewport.Width = pViewport->GetWidth();
+		viewport.Height = pViewport->GetHeight();
+
 		ParaVec3Project(&vOut, &vIn, &viewport, CGUIRoot::GetInstance()->Get3DViewProjMatrix(), NULL, NULL);
 		
 		int nWidth = pIn->rect.right - pIn->rect.left;
