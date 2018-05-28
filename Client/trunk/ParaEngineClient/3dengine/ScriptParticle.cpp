49283f730ca33ed9ef32ee1991cd5d65b83d4c93
devilwalk <devilwalk@163.com>
2018-05-28 10:48:21 +0800

    some bug and ogre billboardset

--- Client/trunk/ParaEngineClient/3dengine/ScriptParticle.cpp
+++ Client/trunk/ParaEngineClient/3dengine/ScriptParticle.cpp
@@ -18,32 +18,52 @@ int ParaEngine::CScriptParticle::InstallFields(CAttributeClass * pClass,bool bOv
 	pClass->AddField("beginParticle",FieldType_void,(void*)beginParticle_s,NULL,NULL,NULL,bOverride);
 	pClass->AddField("endParticle",FieldType_void,(void*)endParticle_s,NULL,NULL,NULL,bOverride);
 	pClass->AddField("mParticlePositionX",FieldType_Float,(void*)setParticlePositionX_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticlePositionY",FieldType_Float,(void*)setParticlePositionY_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticlePositionZ",FieldType_Float,(void*)setParticlePositionZ_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticlePositionZ",FieldType_Float,(void*)setParticlePositionZ_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleUVOffsetX",FieldType_Float,(void*)setParticleUVOffsetX_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleUVOffsetY",FieldType_Float,(void*)setParticleUVOffsetY_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleColourR",FieldType_Float,(void*)setParticleColourR_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleColourG",FieldType_Float,(void*)setParticleColourG_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleColourB",FieldType_Float,(void*)setParticleColourB_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleColourA",FieldType_Float,(void*)setParticleColourA_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mTexture",FieldType_String,(void*)setParticleTexture_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleWidth",FieldType_Float,(void*)setParticleWidth_s,NULL,NULL,NULL,bOverride);
-	pClass->AddField("mParticleHeight",FieldType_Float,(void*)setParticleHeight_s,NULL,NULL,NULL,bOverride);
-	return S_OK;
-}
+		pClass->AddField("mParticlePositionY",FieldType_Float,(void*)setParticlePositionY_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticlePositionZ",FieldType_Float,(void*)setParticlePositionZ_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticlePositionZ",FieldType_Float,(void*)setParticlePositionZ_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleUVOffsetX",FieldType_Float,(void*)setParticleUVOffsetX_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleUVOffsetY",FieldType_Float,(void*)setParticleUVOffsetY_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleUVScaleX",FieldType_Float,(void*)setParticleUVScaleX_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleUVScaleY",FieldType_Float,(void*)setParticleUVScaleY_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleColourR",FieldType_Float,(void*)setParticleColourR_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleColourG",FieldType_Float,(void*)setParticleColourG_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleColourB",FieldType_Float,(void*)setParticleColourB_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleColourA",FieldType_Float,(void*)setParticleColourA_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleWidth",FieldType_Float,(void*)setParticleWidth_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleHeight",FieldType_Float,(void*)setParticleHeight_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mParticleRotation",FieldType_Float,(void*)setParticleRotation_s,NULL,NULL,NULL,bOverride);
 
-ParaEngine::CScriptParticle::CScriptParticle()
-	:mCurrentParticleElement(nullptr)
-{
-}
+		pClass->AddField("mTexture",FieldType_String,(void*)setTexture_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mBillboardType",FieldType_String,(void*)setBillboardType_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonDirectionX",FieldType_Float,(void*)setCommonDirectionX_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonDirectionY",FieldType_Float,(void*)setCommonDirectionY_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonDirectionZ",FieldType_Float,(void*)setCommonDirectionZ_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonUpVectorX",FieldType_Float,(void*)setCommonUpX_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonUpVectorY",FieldType_Float,(void*)setCommonUpY_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mCommonUpVectorZ",FieldType_Float,(void*)setCommonUpZ_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mPointRendering",FieldType_Bool,(void*)setPointRenderingEnable_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mRotationType",FieldType_String,(void*)setRotationType_s,NULL,NULL,NULL,bOverride);
+		pClass->AddField("mAccurateFacing",FieldType_Bool,(void*)setAccurateFacing_s,NULL,NULL,NULL,bOverride);
+		return S_OK;
+	}
 
-ParaEngine::CScriptParticle::~CScriptParticle()
-{
-	clear();
-	for(auto element:mFreeElements)
-		delete element;
-}
+	ParaEngine::CScriptParticle::CScriptParticle()
+		:mCurrentParticleElement(nullptr)
+		,mBillboardType(BBT_POINT)
+		,mCommonDirection(Vector3::UNIT_Z)
+		,mCommonUpVector(Vector3::UNIT_Y)
+		,mPointRendering(false)
+		,mAccurateFacing(false)
+		,mRotationType(BBR_TEXCOORD)
+	{
+	}
+
+	ParaEngine::CScriptParticle::~CScriptParticle()
+	{
+		clear();
+		for(auto element:mFreeElements)
+			delete element;
+	}
 
 HRESULT ParaEngine::CScriptParticle::Draw(SceneState * sceneState)
 {
@@ -101,6 +121,19 @@ const Vector2 & ParaEngine::CScriptParticle::getParticleUVOffset() const
 	return mCurrentParticleElement->mUVOffset;
 }
 
+void ParaEngine::CScriptParticle::setParticleUVScale(float u,float v)
+{
+	assert(mCurrentParticleElement);
+	mCurrentParticleElement->mUVScale=Vector2(u,v);
+}
+
+const Vector2 & ParaEngine::CScriptParticle::getParticleUVScale() const
+{
+	// TODO: 在此处插入 return 语句
+	assert(mCurrentParticleElement);
+	return mCurrentParticleElement->mUVScale;
+}
+
 void ParaEngine::CScriptParticle::setParticleColour(float r,float g,float b,float a)
 {
 	assert(mCurrentParticleElement);
@@ -127,35 +160,99 @@ const Vector2 & ParaEngine::CScriptParticle::getParticleSize() const
 	return mCurrentParticleElement->mSize;
 }
 
-void ParaEngine::CScriptParticle::setParticleTexture(const string & filename)
+void ParaEngine::CScriptParticle::setParticleRotation(float radian)
 {
 	assert(mCurrentParticleElement);
-	mCurrentParticleElement->mTexture=new asset_ptr<TextureEntity>(CGlobals::GetAssetManager()->LoadTexture("",filename,TextureEntity::StaticTexture));
+	mCurrentParticleElement->mRotation=radian;
+}
+
+void ParaEngine::CScriptParticle::setTexture(const string & filename)
+{
+	mTexture=CGlobals::GetAssetManager()->LoadTexture("",filename,TextureEntity::StaticTexture);
+}
+
+void ParaEngine::CScriptParticle::setBillboardType(const std::string & val)
+{
+	if(val=="point")
+	{
+		mBillboardType=BBT_POINT;
+	}
+	else if(val=="oriented_common")
+	{
+		mBillboardType=BBT_ORIENTED_COMMON;
+	}
+	else if(val=="oriented_self")
+	{
+		mBillboardType=BBT_ORIENTED_SELF;
+	}
+	else if(val=="perpendicular_common")
+	{
+		mBillboardType=BBT_PERPENDICULAR_COMMON;
+	}
+	else if(val=="perpendicular_self")
+	{
+		mBillboardType=BBT_PERPENDICULAR_SELF;
+	}
+	else
+	{
+		mBillboardType=BBT_POINT;
+	}
+}
+
+void ParaEngine::CScriptParticle::setCommonDirection(float x,float y,float z)
+{
+	mCommonDirection=Vector3(x,y,z);
+}
+
+void ParaEngine::CScriptParticle::setCommonUp(float x,float y,float z)
+{
+	mCommonUpVector=Vector3(x,y,z);
+}
+
+void ParaEngine::CScriptParticle::setPointRenderingEnable(bool enable)
+{
+	mPointRendering=enable;
+}
+
+void ParaEngine::CScriptParticle::setAccurateFacing(bool facing)
+{
+	mAccurateFacing=facing;
+}
+
+void ParaEngine::CScriptParticle::setRotationType(const std::string & val)
+{
+	if(val=="vertex")
+		mRotationType=BBR_VERTEX;
+	else if(val=="texcoord")
+		mRotationType=BBR_TEXCOORD;
+	else
+		mRotationType=BBR_TEXCOORD;
 }
 
 CScriptParticle::SParticleElement * ParaEngine::CScriptParticle::_createOreRetrieve()
 {
 	if(mFreeElements.empty())
-		mFreeElements.push_back(new SParticleElement);
+		mFreeElements.push_back(new SParticleElement(this));
 	auto ret=mFreeElements.back();
 	mFreeElements.pop_back();
 	return ret;
 }
 
-ParaEngine::CScriptParticle::SParticleElement::SParticleElement()
-	:mTexture(nullptr)
+ParaEngine::CScriptParticle::SParticleElement::SParticleElement(CScriptParticle * container)
+	:mContainer(container)
 	,mPosition(0,0,0)
 	,mSize(0,0)
 	,mUVOffset(0,0)
+	,mUVScale(1,1)
 	,mColour(1,1,1,1)
 	,m_vRenderOffset(0,0,0)
+	,mRotation(0)
 {
 	addref();
 }
 
 ParaEngine::CScriptParticle::SParticleElement::~SParticleElement()
 {
-	delete mTexture;
 }
 
 void ParaEngine::CScriptParticle::SParticleElement::draw(SceneState * sceneState)
@@ -165,23 +262,78 @@ void ParaEngine::CScriptParticle::SParticleElement::draw(SceneState * sceneState
 
 int ParaEngine::CScriptParticle::SParticleElement::RenderParticle(SPRITEVERTEX ** ppVertexBuffer,SceneState * pSceneState)
 {
-	auto billboard=pSceneState->mxView;
-	billboard.setTrans(Vector3::ZERO);
-	billboard=billboard.transpose();
-	DWORD color=Color(mColour);
+	//auto billboard=pSceneState->mxView;
+	//billboard.setTrans(Vector3::ZERO);
+	//billboard=billboard.transpose();
+	//DWORD color=Color(mColour);
 	SPRITEVERTEX* pVertexBuffer=*ppVertexBuffer;
-	BillBoardViewInfo& bbInfo=pSceneState->BillBoardInfo();
+	//BillBoardViewInfo& bbInfo=pSceneState->BillBoardInfo();
 
-	const Vector3 world_pos(mPosition+m_vRenderOffset-CGlobals::GetScene()->GetRenderOrigin());
+	//const Vector3 world_pos(mPosition+m_vRenderOffset-CGlobals::GetScene()->GetRenderOrigin());
 
-	Vector3 v[]={Vector3(-mSize.x/2,-mSize.y/2,0),Vector3(-mSize.x/2,mSize.y/2,0),Vector3(mSize.x/2,mSize.y/2,0),Vector3(mSize.x/2,-mSize.y/2,0)};
-	const Vector2 uv[]={Vector2(mUVOffset.x,mUVOffset.y+1.0f),Vector2(mUVOffset.x,mUVOffset.y),Vector2(mUVOffset.x+1.0f,mUVOffset.y),Vector2(mUVOffset.x+1.0f,mUVOffset.y+1.0f)};
-	for(int i=0;i<4;++i)
+	//Vector3 v[]={Vector3(-mSize.x/2,-mSize.y/2,0),Vector3(-mSize.x/2,mSize.y/2,0),Vector3(mSize.x/2,mSize.y/2,0),Vector3(mSize.x/2,-mSize.y/2,0)};
+	//const Vector2 uv[]={Vector2(mUVOffset.x,mUVOffset.y+1.0f),Vector2(mUVOffset.x,mUVOffset.y),Vector2(mUVOffset.x+1.0f,mUVOffset.y),Vector2(mUVOffset.x+1.0f,mUVOffset.y+1.0f)};
+	//for(int i=0;i<4;++i)
+	//{
+	//	Vector3 v_t;
+	//	ParaVec3TransformCoord(&v_t,&v[i],&billboard);
+	//	auto wpos=world_pos+v_t;
+	//	SetParticleVertex(pVertexBuffer[i],wpos.x,wpos.y,wpos.z,uv[i].x,uv[i].y,color);
+	//}
+
+	float left=-0.5f;
+	float right=0.5f;
+	float top=0.5f;
+	float bottom=-0.5f;
+	Vector3 camere_x(Vector3::ZERO),camera_y(Vector3::ZERO);
+	Vector3 v_offset[4];
+	if(!mContainer->mPointRendering)
 	{
-		Vector3 v_t;
-		ParaVec3TransformCoord(&v_t,&v[i],&billboard);
-		auto wpos=world_pos+v_t;
-		SetParticleVertex(pVertexBuffer[i],wpos.x,wpos.y,wpos.z,uv[i].x,uv[i].y,color);
+		// Generate axes etc up-front if not oriented per-billboard
+		if(mContainer->mBillboardType!=BBT_ORIENTED_SELF&&
+			mContainer->mBillboardType!=BBT_PERPENDICULAR_SELF&&
+			!(mContainer->mAccurateFacing && mContainer->mBillboardType!=BBT_PERPENDICULAR_COMMON))
+		{
+			genBillboardAxes(&camere_x,&camera_y,pSceneState);
+
+			/* If all billboards are the same size we can precalculate the
+			offsets and just use '+' instead of '*' for each billboard,
+			and it should be faster.
+			*/
+			genVertOffsets(left,right,top,bottom,
+				mSize.x,mSize.y,camere_x,camera_y,v_offset);
+
+		}
+	}
+	// If they're all the same size or we're point rendering
+	if(mContainer->mPointRendering)
+	{
+		/* No per-billboard checking, just blast through.
+		Saves us an if clause every billboard which may
+		make a difference.
+		*/
+
+		genVertices(v_offset,pVertexBuffer);
+	}
+	else // not all default size and not point rendering
+	{
+		Vector3 vOwnOffset[4];
+		// If it has own dimensions, or self-oriented, gen offsets
+		if(mContainer->mBillboardType==BBT_ORIENTED_SELF||
+			mContainer->mBillboardType==BBT_PERPENDICULAR_SELF||
+			true/*bb.mOwnDimensions*/||
+			(mContainer->mAccurateFacing && mContainer->mBillboardType!=BBT_PERPENDICULAR_COMMON))
+		{
+			// Generate using own dimensions
+			genVertOffsets(left,right,top,bottom,
+				mSize.x,mSize.y,camere_x,camera_y,vOwnOffset);
+			// Create vertex data
+			genVertices(vOwnOffset,pVertexBuffer);
+		}
+		else // Use default dimension, already computed before the loop, for faster creation
+		{
+			genVertices(v_offset,pVertexBuffer);
+		}
 	}
 
 	pVertexBuffer[4]=pVertexBuffer[0];
@@ -192,8 +344,194 @@ int ParaEngine::CScriptParticle::SParticleElement::RenderParticle(SPRITEVERTEX *
 
 TextureEntity * ParaEngine::CScriptParticle::SParticleElement::GetTexture()
 {
-	if(mTexture)
-		return mTexture->get();
+	return mContainer->mTexture.get();
+}
+
+void ParaEngine::CScriptParticle::SParticleElement::genVertOffsets(float inleft,float inright,float intop,float inbottom,float width,float height,const Vector3 & x,const Vector3 & y,Vector3 * pDestVec)
+{
+	Vector3 vLeftOff,vRightOff,vTopOff,vBottomOff;
+	/* Calculate default offsets. Scale the axes by
+	parametric offset and dimensions, ready to be added to
+	positions.
+	*/
+
+	vLeftOff=x*(inleft   * width);
+	vRightOff=x*(inright  * width);
+	vTopOff=y*(intop   * height);
+	vBottomOff=y*(inbottom * height);
+
+	// Make final offsets to vertex positions
+	pDestVec[1]=vLeftOff+vTopOff;
+	pDestVec[2]=vRightOff+vTopOff;
+	pDestVec[0]=vLeftOff+vBottomOff;
+	pDestVec[3]=vRightOff+vBottomOff;
+}
+
+void ParaEngine::CScriptParticle::SParticleElement::genBillboardAxes(Vector3 * pX,Vector3 * pY,SceneState * pSceneState)
+{
+	const Vector3 world_pos(mPosition+m_vRenderOffset);
+	Matrix3 rot_view;
+	pSceneState->mxView.extract3x3Matrix(rot_view);
+	Vector3 mCamDir=rot_view.GetColumn(2);
+	// If we're using accurate facing, recalculate camera direction per BB
+	if(mContainer->mAccurateFacing&&
+		(mContainer->mBillboardType==BBT_POINT||
+			mContainer->mBillboardType==BBT_ORIENTED_COMMON||
+			mContainer->mBillboardType==BBT_ORIENTED_SELF))
+	{
+		// cam -> bb direction
+		mCamDir=world_pos-pSceneState->vEye;
+		mCamDir.normalise();
+	}
+	Vector3 vUp=rot_view.GetColumn(1);
+	Vector3 vRight=vUp.crossProduct(mCamDir);
+	vUp=mCamDir.crossProduct(vRight);
+	Quaternion mCamQ=Quaternion(vRight,vUp,mCamDir).Inverse();
+
+
+	switch(mContainer->mBillboardType)
+	{
+	case BBT_POINT:
+		if(mContainer->mAccurateFacing)
+		{
+			// Point billboards will have 'up' based on but not equal to cameras
+			// Use pY temporarily to avoid allocation
+			*pY=mCamQ*Vector3::UNIT_Y;
+			*pX=mCamDir.crossProduct(*pY);
+			pX->normalise();
+			*pY=pX->crossProduct(mCamDir); // both normalised already
+		}
+		else
+		{
+			// Get camera axes for X and Y (depth is irrelevant)
+			*pX=mCamQ*Vector3::UNIT_X;
+			*pY=mCamQ*Vector3::UNIT_Y;
+		}
+		break;
+
+	case BBT_ORIENTED_COMMON:
+		// Y-axis is common direction
+		// X-axis is cross with camera direction
+		*pY=mContainer->mCommonDirection;
+		*pX=mCamDir.crossProduct(*pY);
+		pX->normalise();
+		break;
+
+	case BBT_ORIENTED_SELF:
+		// Y-axis is direction
+		// X-axis is cross with camera direction
+		// Scale direction first
+		*pY=Vector3::UNIT_Z;
+		*pX=mCamDir.crossProduct(*pY);
+		pX->normalise();
+		break;
+
+	case BBT_PERPENDICULAR_COMMON:
+		// X-axis is up-vector cross common direction
+		// Y-axis is common direction cross X-axis
+		*pX=mContainer->mCommonUpVector.crossProduct(mContainer->mCommonDirection);
+		*pY=mContainer->mCommonDirection.crossProduct(*pX);
+		break;
+
+	case BBT_PERPENDICULAR_SELF:
+		// X-axis is up-vector cross own direction
+		// Y-axis is own direction cross X-axis
+		*pX=mContainer->mCommonUpVector.crossProduct(Vector3::UNIT_Z);
+		pX->normalise();
+		*pY=Vector3::UNIT_Z.crossProduct(*pX); // both should be normalised
+		break;
+	}
+}
+
+void ParaEngine::CScriptParticle::SParticleElement::genVertices(const Vector3 * const offsets,SPRITEVERTEX * vertex)
+{
+	DWORD color=Color(mColour);
+	Vector3 v[]={offsets[0],offsets[1],offsets[2],offsets[3]};
+	Vector2 uv[]={Vector2(0,1.0f),Vector2(0,0),Vector2(1.0f,0),Vector2(1.0f,1.0f)};
+	const Vector3 world_pos(mPosition+m_vRenderOffset-CGlobals::GetScene()->GetRenderOrigin());
+
+	if(mRotation==0)
+	{
+		for(int i=0;i<4;++i)
+		{
+			uv[i]*=mUVScale;
+			uv[i]+=mUVOffset;
+			// Left-top
+			// Positions
+			vertex[i].p.x=v[i].x+world_pos.x;
+			vertex[i].p.y=v[i].y+world_pos.y;
+			vertex[i].p.z=v[i].z+world_pos.z;
+			vertex[i].color=color;
+			vertex[i].tu=uv[i].x;
+			vertex[i].tv=uv[i].y;
+		}
+	}
+	else if(mContainer->mRotationType==BBR_VERTEX)
+	{
+		// TODO: Cache axis when billboard type is BBT_POINT or BBT_PERPENDICULAR_COMMON
+		Vector3 axis=(offsets[3]-offsets[0]).crossProduct(offsets[2]-offsets[1]).normalisedCopy();
+
+		Quaternion rotation;
+		rotation.FromAngleAxis(Radian(mRotation),axis);
+
+		for(int i=0;i<4;++i)
+		{
+			uv[i]*=mUVScale;
+			uv[i]+=mUVOffset;
+			v[i]=rotation*v[i];
+			// Left-top
+			// Positions
+			vertex[i].p.x=v[i].x+world_pos.x;
+			vertex[i].p.y=v[i].y+world_pos.y;
+			vertex[i].p.z=v[i].z+world_pos.z;
+			vertex[i].color=color;
+			vertex[i].tu=uv[i].x;
+			vertex[i].tv=uv[i].y;
+		}
+	}
 	else
-		return nullptr;
+	{
+		const float      cos_rot(Math::Cos(mRotation));
+		const float      sin_rot(Math::Sin(mRotation));
+
+		float width=0.5f;
+		float height=0.5f;
+		float mid_u=0.5f;
+		float mid_v=0.5f;
+
+		float cos_rot_w=cos_rot*width;
+		float cos_rot_h=cos_rot*height;
+		float sin_rot_w=sin_rot*width;
+		float sin_rot_h=sin_rot*height;
+
+		// Texture coords
+		uv[0].x=mid_u-cos_rot_w+sin_rot_h;
+		uv[0].y=mid_v-sin_rot_w-cos_rot_h;
+
+		// Texture coords
+		uv[1].x=mid_u+cos_rot_w+sin_rot_h;
+		uv[1].y=mid_v+sin_rot_w-cos_rot_h;
+
+		// Texture coords
+		uv[2].x=mid_u-cos_rot_w-sin_rot_h;
+		uv[2].y=mid_v-sin_rot_w+cos_rot_h;
+
+		// Texture coords
+		uv[3].x=mid_u+cos_rot_w-sin_rot_h;
+		uv[3].y=mid_v+sin_rot_w+cos_rot_h;
+
+		for(int i=0;i<4;++i)
+		{
+			uv[i]*=mUVScale;
+			uv[i]+=mUVOffset;
+			// Left-top
+			// Positions
+			vertex[i].p.x=v[i].x+world_pos.x;
+			vertex[i].p.y=v[i].y+world_pos.y;
+			vertex[i].p.z=v[i].z+world_pos.z;
+			vertex[i].color=color;
+			vertex[i].tu=uv[i].x;
+			vertex[i].tv=uv[i].y;
+		}
+	}
 }
--- Client/trunk/ParaEngineClient/3dengine/ScriptParticle.h
+++ Client/trunk/ParaEngineClient/3dengine/ScriptParticle.h
@@ -8,16 +8,38 @@ namespace ParaEngine
 		:public CTileObject
 	{
 	public:
+		enum BillboardRotationType
+		{
+			/// Rotate the billboard's vertices around their facing direction
+			BBR_VERTEX,
+			/// Rotate the billboard's texture coordinates
+			BBR_TEXCOORD
+		};
+		enum BillboardType
+		{
+			/// Standard point billboard (default), always faces the camera completely and is always upright
+			BBT_POINT,
+			/// Billboards are oriented around a shared direction vector (used as Y axis) and only rotate around this to face the camera
+			BBT_ORIENTED_COMMON,
+			/// Billboards are oriented around their own direction vector (their own Y axis) and only rotate around this to face the camera
+			BBT_ORIENTED_SELF,
+			/// Billboards are perpendicular to a shared direction vector (used as Z axis, the facing direction) and X, Y axis are determined by a shared up-vertor
+			BBT_PERPENDICULAR_COMMON,
+			/// Billboards are perpendicular to their own direction vector (their own Z axis, the facing direction) and X, Y axis are determined by a shared up-vertor
+			BBT_PERPENDICULAR_SELF
+		};
 		struct SParticleElement
 			:public CParticleElement
 		{
+			CScriptParticle * const mContainer;
 			Vector3 mPosition;
 			Vector2 mSize;
 			Vector2 mUVOffset;
+			Vector2 mUVScale;
 			LinearColor mColour;
-			asset_ptr<TextureEntity> * mTexture;
 			Vector3 m_vRenderOffset;
-			SParticleElement();
+			float mRotation;
+			SParticleElement(CScriptParticle * container);
 			~SParticleElement();
 			void draw(SceneState * sceneState);
 			// Í¨¹ý CParticleElement ¼Ì³Ð
@@ -26,11 +48,24 @@ namespace ParaEngine
 		protected:
 			ParaEngine::Vector3 GetRenderOffset() const { return m_vRenderOffset; }
 			void SetRenderOffset(ParaEngine::Vector3 val) { m_vRenderOffset=val; }
+
+			void genVertOffsets(float inleft,float inright,float intop,float inbottom,
+				float width,float height,const Vector3& x,const Vector3& y,Vector3* pDestVec);
+			void genBillboardAxes(Vector3* pX,Vector3 *pY,SceneState * pSceneState);
+			void genVertices(const Vector3* const offsets,SPRITEVERTEX * vertex);
 		};
 	protected:
 		std::vector<SParticleElement*> mActiveElements;
 		std::vector<SParticleElement*> mFreeElements;
 		SParticleElement * mCurrentParticleElement;
+
+		asset_ptr<TextureEntity> mTexture;
+		BillboardType mBillboardType;
+		Vector3 mCommonDirection;
+		Vector3 mCommonUpVector;
+		bool mPointRendering;
+		bool mAccurateFacing;
+		BillboardRotationType mRotationType;
 	public:
 		ATTRIBUTE_DEFINE_CLASS(CScriptParticle);
 		ATTRIBUTE_SUPPORT_CREATE_FACTORY(CScriptParticle);
@@ -43,13 +78,27 @@ namespace ParaEngine
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticlePositionZ_s,float)	{ cls->setParticlePosition(cls->getParticlePosition().x,cls->getParticlePosition().y,p1); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleUVOffsetX_s,float)	{ cls->setParticleUVOffset(p1,cls->getParticleUVOffset().y); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleUVOffsetY_s,float)	{ cls->setParticleUVOffset(cls->getParticleUVOffset().x,p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setParticleUVScaleX_s,float)	{ cls->setParticleUVScale(p1,cls->getParticleUVScale().y); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setParticleUVScaleY_s,float)	{ cls->setParticleUVScale(cls->getParticleUVScale().x,p1); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleColourR_s,float)	{ cls->setParticleColour(p1,cls->getParticleColour().g,cls->getParticleColour().b,cls->getParticleColour().a); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleColourG_s,float)	{ cls->setParticleColour(cls->getParticleColour().r,p1,cls->getParticleColour().b,cls->getParticleColour().a); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleColourB_s,float)	{ cls->setParticleColour(cls->getParticleColour().r,cls->getParticleColour().g,p1,cls->getParticleColour().a); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleColourA_s,float)	{ cls->setParticleColour(cls->getParticleColour().r,cls->getParticleColour().g,cls->getParticleColour().b,p1); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleWidth_s,float)	{ cls->setParticleSize(p1,cls->getParticleSize().y); return S_OK; }
 		ATTRIBUTE_METHOD1(CScriptParticle,setParticleHeight_s,float)	{ cls->setParticleSize(cls->getParticleSize().x,p1); return S_OK; }
-		ATTRIBUTE_METHOD1(CScriptParticle,setParticleTexture_s,const char*)	{ cls->setParticleTexture(p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setParticleRotation_s,float)	{ cls->setParticleRotation(p1); return S_OK; }
+
+		ATTRIBUTE_METHOD1(CScriptParticle,setTexture_s,const char*)	{ cls->setTexture(p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setBillboardType_s,const char*)	{ cls->setBillboardType(p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonDirectionX_s,float)	{ cls->setCommonDirection(p1,cls->getCommonDirection().y,cls->getCommonDirection().z); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonDirectionY_s,float)	{ cls->setCommonDirection(cls->getCommonDirection().x,p1,cls->getCommonDirection().z); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonDirectionZ_s,float)	{ cls->setCommonDirection(cls->getCommonDirection().x,cls->getCommonDirection().y,p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonUpX_s,float)	{ cls->setCommonUp(p1,cls->getCommonUp().y,cls->getCommonUp().z); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonUpY_s,float)	{ cls->setCommonUp(cls->getCommonUp().x,p1,cls->getCommonUp().z); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setCommonUpZ_s,float)	{ cls->setCommonUp(cls->getCommonUp().x,cls->getCommonUp().y,p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setPointRenderingEnable_s,bool)	{ cls->setPointRenderingEnable(p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setAccurateFacing_s,bool)	{ cls->setAccurateFacing(p1); return S_OK; }
+		ATTRIBUTE_METHOD1(CScriptParticle,setRotationType_s,const char*)	{ cls->setRotationType(p1); return S_OK; }
 		CScriptParticle();
 		~CScriptParticle();
 
@@ -62,11 +111,23 @@ namespace ParaEngine
 		const Vector3 & getParticlePosition()const;
 		void setParticleUVOffset(float u,float v);
 		const Vector2 & getParticleUVOffset()const;
+		void setParticleUVScale(float u,float v);
+		const Vector2 & getParticleUVScale()const;
 		void setParticleColour(float r,float g,float b,float a);
 		const LinearColor & getParticleColour()const;
 		void setParticleSize(float width,float height);
 		const Vector2 & getParticleSize()const;
-		void setParticleTexture(const string & filename);
+		void setParticleRotation(float radian);
+
+		void setTexture(const string & filename);
+		void setBillboardType(const std::string & type);
+		void setCommonDirection(float x,float y,float z);
+		const Vector3 & getCommonDirection()const{ return mCommonDirection; }
+		void setCommonUp(float x,float y,float z);
+		const Vector3 & getCommonUp()const{ return mCommonUpVector; }
+		void setPointRenderingEnable(bool enable);
+		void setAccurateFacing(bool facing);
+		void setRotationType(const std::string & type);
 
 	protected:
 		SParticleElement*_createOreRetrieve();
