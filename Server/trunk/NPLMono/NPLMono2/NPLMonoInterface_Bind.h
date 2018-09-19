09015d9166ade0b6b3e2bccc5fd17b0f61fab0b6
kkvskkkk <460864076@qq.com>
2018-09-19 10:47:35 +0800

    * move mono

--- Server/trunk/NPLMono/NPLMono2/NPLMonoInterface_Bind.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/** add internal calls to mono. To add a new function in NPLMonoInterface.cpp.
-	1. provide the function wrapper in NPLMonoInterface.cpp 
-	1. add the binding in NPLMonoInterface_bind.cpp
-	1. edit NPLMonoInterface.cs to tell the scripting interface 
-*/
-extern void Add_NPL_Internal_Call();
