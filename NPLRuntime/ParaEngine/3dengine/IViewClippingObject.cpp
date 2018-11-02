895c57e97d908b20090982fae039af297c6487ae
LiXizhi <lixizhi@yeah.net>
2018-11-02 17:10:27 +0800

    fix a tiling height->depth issue

--- NPLRuntime/ParaEngine/3dengine/IViewClippingObject.cpp
+++ NPLRuntime/ParaEngine/3dengine/IViewClippingObject.cpp
@@ -712,7 +712,7 @@ void ParaEngine::IViewClippingObject::GetFacing3D( Vector3 *pV )
 void ParaEngine::IViewClippingObject::GetBoundRect( FLOAT* fWidth ,FLOAT* fHeight, FLOAT* fFacing )
 {
 	*fWidth = GetWidth();
-	*fHeight = GetHeight();
+	*fHeight = GetDepth();
 	*fFacing = GetFacing();
 }
 
