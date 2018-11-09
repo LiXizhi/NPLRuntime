39cf95985b38510fa976e78338457d4cc5c765d7
kkvskkkk <460864076@qq.com>
2018-11-09 16:50:34 +0800

    * fixed link error

--- NPLRuntime/Platform/Android/src/main.cpp
+++ NPLRuntime/Platform/Android/src/main.cpp
@@ -10,6 +10,25 @@
 #include <android/log.h>
 
 
+/*
+	fixed link error with luajit2.0
+*/
+#ifdef NOT_HAVE_SWBUF
+extern "C" int __swbuf(int c, FILE *stream)
+{
+	return putc(c, stream);
+}
+#endif
+
+#ifdef NOT_HAVE_SRGET
+extern "C" int __srget(FILE *stream)
+{
+	return getc(stream);
+}
+#endif
+
+
+
 
 /**
  * This is the main entry point of a native application that is using
