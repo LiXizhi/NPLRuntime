f8967bc1a8faa24eb23140a851f9b4c10877de71
LiXizhi <lxz1982@hotmail.com>
2018-12-27 22:25:59 +0800

    minor fix

--- build_android_tegra.bat
+++ build_android_tegra.bat
@@ -1,7 +1,17 @@
-pushd .
-rem Build android 
-mkdir build\android_tegra\
-cd build\android_tegra
+@echo off
+echo building NPLRuntime for android with clang toolchain
+echo NDK_ROOT: %NDK_ROOT%
+echo BOOST_ROOT (optional): %BOOST_ROOT%
+echo also need cmake, Python for windows, visual studio and Nsight tegra 3.5
 
+rem prepare clang toolchain and build boost
+pushd NPLRuntime\externals\boost\
+call build_android.bat
+popd
+
+pushd .
+echo generate visual studio solution with cmake  
+if not exist build\android_tegra\ ( mkdir build\android_tegra\ )
+cd build\android_tegra
 call "cmake.exe" -G "Visual Studio 15 2017" -T clang-3.8 -DCMAKE_SYSTEM_NAME=Android -DNPLRUNTIME_PROJECT_NAME=NPLRuntimeAndroid ../../NPLRuntime
 popd
