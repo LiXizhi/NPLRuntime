5a7958c6ec7709b90d16980d80bed83d8adb67cd
imfool <imfool@me.com>
2019-01-11 16:55:38 +0800

    mac problems fix by xizhi

--- NPLRuntime/externals/boost/build_ios_and_osx.sh
+++ NPLRuntime/externals/boost/build_ios_and_osx.sh
@@ -3,5 +3,5 @@
 pushd .
 mkdir -p ./prebuild/apple
 cd prebuild/apple
-sh ../../scripts/boost.sh -ios -macos  --no-framework --boost-version 1.64.0 --boost-libs "thread date_time filesystem system chrono signals regex serialization iostreams log"
+sh ../../scripts/boost.sh -ios -macos  --no-framework --boost-version 1.69.0 --boost-libs "thread date_time filesystem system chrono regex serialization iostreams log"
 popd
