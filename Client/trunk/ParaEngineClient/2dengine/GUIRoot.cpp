bbc45470f180ea9b497747c8644c5eae4c18aa80
LXZ_svn_sync <lixizhi@yeah.net>
2019-05-09 17:49:28 +0800

    fix right click key focus and added GUIRoot:Focus to set real key focus back

--- Client/trunk/ParaEngineClient/2dengine/GUIRoot.cpp
+++ Client/trunk/ParaEngineClient/2dengine/GUIRoot.cpp
@@ -1989,6 +1989,14 @@ bool ParaEngine::CGUIRoot::CheckLoadCursor()
 	return true;
 }
 
+void ParaEngine::CGUIRoot::Focus()
+{
+	CGUIContainer::Focus();
+#ifdef WIN32
+	CGlobals::GetApp()->PostWinThreadMessage(PE_WM_SETFOCUS, (WPARAM)(CGlobals::GetAppHWND()), 0);
+#endif
+}
+
 void CGUIRoot::SetMouseInClient(bool bMouseInClient)
 {
 #ifdef PARAENGINE_MOBILE
