a6340ea88bcec206fd537a2ccbe3a8df2a9e669e
onedou <onedou@126.com>
2019-09-05 11:14:47 +0800

    add create sub view view

--- NPLRuntime/Platform/OSX/src/WebView/WebView.mm
+++ NPLRuntime/Platform/OSX/src/WebView/WebView.mm
@@ -23,8 +23,6 @@
 @interface WebViewWindowController () <WKNavigationDelegate, WKUIDelegate>
 {
     WKWebView *webView;
-
-    
 }
 @property (assign) IBOutlet WKWebView *webView;
 //@property (nonatomic) std::function<void()> onCloseCallback;
@@ -72,7 +70,12 @@ namespace ParaEngine {
     {
         return ParaEngineWebView::createWebView(x, y, w, h);
     }
-    
+
+    IParaWebView* IParaWebView::createSubViewView()
+    {
+        return ParaEngineWebView::createWebView(0, 0, 100, 100);
+    }
+
     ParaEngineWebView* ParaEngineWebView::createWebView(int x, int y, int w, int h)
     {
         auto  p = new ParaEngineWebView();
