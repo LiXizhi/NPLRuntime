fee5fb5a1a6c1f6506287a0e7dc0d42018e2a016
kkvskkkk <460864076@qq.com>
2020-06-15 16:09:24 +0800

    * supported machine id on mac

--- NPLRuntime/Platform/OSX/src/3dengine/ParaEngineSettings.mm
+++ NPLRuntime/Platform/OSX/src/3dengine/ParaEngineSettings.mm
@@ -4,10 +4,51 @@
 // Emails:	onedou@126.com
 // Date:	2020.6.11
 //-----------------------------------------------------------------------------
+#import <Cocoa/Cocoa.h>
+
 #include "ParaEngine.h"
 #include "ParaEngineSettings.h"
 
-const std::string& ParaEngine::ParaEngineSettings::GetMachineID()
-{
-	return "";
-}
+namespace ParaEngine {
+
+    const std::string& ParaEngineSettings::GetMachineID()
+    {
+        static std::string str = "";
+        
+        if (str.empty())
+        {
+            NSTask *task = [[NSTask alloc] init];
+            [task setLaunchPath:@"/usr/sbin/ioreg"];
+            
+            NSArray *arguments = [NSArray arrayWithObjects:@"-rd1", @"-c", @"IOPlatformExpertDevice", nil];
+            [task setArguments:arguments];
+            
+            NSPipe *pipe = [NSPipe pipe];
+            [task setStandardOutput:pipe];
+            
+            NSFileHandle *file = [pipe fileHandleForReading];
+            
+            [task launch];
+            
+            NSData *data = [file readDataToEndOfFile];
+            
+            NSString *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            
+            NSString *key = @"IOPlatformUUID";
+            NSRange range = [string rangeOfString:key];
+            
+            NSInteger location = range.location + [key length] + 5;
+            NSInteger length = 32 + 4;
+            range.location =location;
+            range.length = length;
+            
+            NSString *UUID = [string substringWithRange:range];
+            
+            str = [UUID UTF8String];
+        }
+        
+        return str;
+    }
+
+
+} // namespace ParaEngine
