175cfe1fb80630255333e1d846c796d28cebe1bc
onedou <onedou@126.com>
2020-06-15 13:53:09 +0800

    update

---
+++ NPLRuntime/Platform/Linux/src/3dengine/ParaEngineSettings.cpp
@@ -0,0 +1,13 @@
+//-----------------------------------------------------------------------------
+// Class:	Linux ParaEngineSettings
+// Authors:	big
+// Emails:	onedou@126.com
+// Date:	2020.6.15
+//-----------------------------------------------------------------------------
+#include "ParaEngine.h"
+#include "ParaEngineSettings.h"
+
+const std::string& ParaEngine::ParaEngineSettings::GetMachineID()
+{
+	return "";
+}
