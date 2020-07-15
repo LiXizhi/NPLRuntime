1becea5a0fa72e41ecf65318896d5197f0fd1399
kkvskkkk <460864076@qq.com>
2020-07-15 18:04:35 +0800

    add nploce

---
+++ NPLRuntime/Plugins/NplOce/PlaceHolder.cpp
@@ -0,0 +1,14 @@
+void NplOce_PlaceHolder();
+
+namespace NplOcePlaceHolder{
+	class __PlaceHolder
+	{
+	public:
+		__PlaceHolder()
+		{
+			NplOce_PlaceHolder();
+		}
+	};
+	
+	__PlaceHolder g_placeHolder;
+}
