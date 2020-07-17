a4171aff8d867f160f1cbf7667f828f957a8faeb
kkvskkkk <460864076@qq.com>
2020-07-17 09:58:32 +0800

    update nploce plugin

--- NPLRuntime/Platform/Windows/src/main.cpp
+++ NPLRuntime/Platform/Windows/src/main.cpp
@@ -28,7 +28,6 @@
 using namespace ParaEngine;
 extern HINSTANCE g_hAppInstance;
 
-extern "C" __declspec(dllexport) const char* LibDescription();
 
 INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR lpCmdLine, INT )
 {
