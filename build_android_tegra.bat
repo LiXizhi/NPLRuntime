aa2b5ecf7051e67fde7b10dc77664c211cd2e83b
kkvskkkk <460864076@qq.com>
2020-07-20 14:55:23 +0800

    remove old files

--- build_android_tegra.bat
+++
@@ -1,7 +0,0 @@
-pushd .
-rem Build android 
-mkdir bin\android_tegra\
-cd bin\android_tegra
-
-call "cmake.exe" -G "Visual Studio 15 2017" -DCMAKE_SYSTEM_NAME=Android -DNPLRUNTIME_PROJECT_NAME=NPLRuntime_android ../../NPLRuntime
-popd
