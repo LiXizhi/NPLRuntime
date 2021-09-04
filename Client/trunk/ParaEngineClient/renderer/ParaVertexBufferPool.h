0beec9d26cf05f0f64946d76dfece77c4c5a8797
LXZ_svn_sync <lixizhi@yeah.net>
2021-09-04 12:18:06 +0800

    minor Fix typo

--- Client/trunk/ParaEngineClient/renderer/ParaVertexBufferPool.h
+++ Client/trunk/ParaEngineClient/renderer/ParaVertexBufferPool.h
@@ -57,7 +57,7 @@ namespace ParaEngine
 		/** clear all pooled objects. */
 		virtual void Cleanup();
 	
-		virtual HRESULT DeleteDeviceObjects();;
+		virtual HRESULT DeleteDeviceObjects();
 		virtual HRESULT RendererRecreated();
 		
 		
