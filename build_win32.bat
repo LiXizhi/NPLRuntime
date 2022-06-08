3de36948129449986021ff4e64ca5561f682a946
wxaxiaoyao <765485868@qq.com>
2022-06-08 08:58:27 +0800

    update github ci

--- build_win32.bat
+++ build_win32.bat
@@ -14,7 +14,7 @@ rem Build main executable
 mkdir build\win32
 cd build\win32
 
-if NOT "%GITHUB_WORKFLOW%" == "" ("C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat x86")
+if NOT "%GITHUB_WORKFLOW%" == "" (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86)
 
 @echo on
 echo %BOOST_ROOT%
