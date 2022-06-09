748ac1b8fca76bb12e33645d9c4bb5a60ed2cef6
wxaxiaoyao <765485868@qq.com>
2022-06-09 14:57:17 +0800

    update ci

--- build_win32_github_workflows.bat
+++ build_win32_github_workflows.bat
@@ -1,17 +1,18 @@
 
 
-if EXIST "boost.7z" (
-    set "PATH=%PATH%;C:\Program Files\7-Zip"
-	7z x boost.7z -obin > nul
-	move boost_* boost
-	cd boost
-	bootstrap.bat
-	b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-    set BOOST_ROOT=%~dp0bin\boost
-    echo %BOOST_ROOT%
-)
 
 if NOT "%GITHUB_WORKFLOW%" == "" (
+    
+    set "PATH=%PATH%;C:\Program Files\7-Zip"
+    powershell -Command "Invoke-WebRequest https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.7z -OutFile boost.7z"
+    7z x boost.7z -obin > nul
+    move boost_* boost
+    cd boost
+    bootstrap.bat
+    b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+    set BOOST_ROOT=%cd%\boost
+    echo %BOOST_ROOT%
+
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
     mkdir build\win32
     cd build\win32
