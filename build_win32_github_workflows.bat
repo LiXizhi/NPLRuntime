ad215369643e1a383c28a4c594e9d4ca6631dc75
wxaxiaoyao <765485868@qq.com>
2022-06-09 18:14:55 +0800

    update ci

--- build_win32_github_workflows.bat
+++ build_win32_github_workflows.bat
@@ -1,11 +1,12 @@
 
+git submodule init
 
+git submodule update --init --recursive
 
 if NOT "%GITHUB_WORKFLOW%" == "" (
     if EXIST "boost_1_78_0.7z" (
         set "PATH=%PATH%;C:\Program Files\7-Zip"
-        REM powershell -Command "Invoke-WebRequest https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.7z -OutFile boost.7z"
-        7z x boost_1_78_0.7z 
+        7x x boost_1_78_0.7z 
         cd boost_1_78_0
         bootstrap.bat
         b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
@@ -14,11 +15,28 @@ if NOT "%GITHUB_WORKFLOW%" == "" (
         cd ..
     )
 
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"  AMD64
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
     mkdir build\win32
     cd build\win32
     cmake ..\..\Client
-    cat D:/a/NPLRuntime/NPLRuntime/build/win32/CMakeFiles/CMakeError.log
     REM cat D:/a/NPLRuntime/NPLRuntime/build/win32/CMakeFiles/CMakeOutput.log
     REM msbuild .\CLIENT.sln /verbosity:minimal /property:Configuration=Release
+) else (
+    if EXIST "boost_1_78_0.7z" (
+        .\bin\7z.exe x boost_1_78_0.7z 
+        cd boost_1_78_0
+        bootstrap.bat
+        b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+        b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+        set BOOST_ROOT=%cd%\boost_1_78_0
+        cd ..
+    )
+
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+    mkdir build\win32
+    cd build\win32
+    cmake ..\..\Client
+    REM cat D:/a/NPLRuntime/NPLRuntime/build/win32/CMakeFiles/CMakeOutput.log
+    REM msbuild .\CLIENT.sln /verbosity:minimal /property:Configuration=Release
+
 )
