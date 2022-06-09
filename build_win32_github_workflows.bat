e4ab6ba28dbbeb5a9b6ba0d39c3d1d941c987bc6
wxaxiaoyao <765485868@qq.com>
2022-06-09 14:47:25 +0800

    update code

--- build_win32_github_workflows.bat
+++ build_win32_github_workflows.bat
@@ -1,4 +1,16 @@
 
+
+if EXIST "boost.7z" (
+    set "PATH=%PATH%;C:\Program Files\7-Zip"
+	7z x boost.7z -obin > nul
+	move boost_* boost
+	cd boost
+	bootstrap.bat
+	b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+    set BOOST_ROOT=%~dp0bin\boost
+    echo %BOOST_ROOT%
+)
+
 if NOT "%GITHUB_WORKFLOW%" == "" (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
     mkdir build\win32
