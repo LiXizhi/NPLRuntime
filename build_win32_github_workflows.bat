5bd3b0691680824d78e76d0a97928907340a85a6
wxaxiaoyao <765485868@qq.com>
2022-06-09 18:22:34 +0800

    update ci

--- build_win32_github_workflows.bat
+++ build_win32_github_workflows.bat
@@ -35,7 +35,7 @@ if NOT "%GITHUB_WORKFLOW%" == "" (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
     mkdir build\win32
     cd build\win32
-    cmake ..\..\Client
+    cmake ..\..\Client -DUSE_OPENGL_RENDERER:BOOL=TRUE
     REM cat D:/a/NPLRuntime/NPLRuntime/build/win32/CMakeFiles/CMakeOutput.log
     REM msbuild .\CLIENT.sln /verbosity:minimal /property:Configuration=Release
 
