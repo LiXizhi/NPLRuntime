462b67d2437663ecea94b3a38973ae11d421bab6
wxaxiaoyao <765485868@qq.com>
2022-06-10 18:11:44 +0800

    update ci

--- build_boost.bat
+++ build_boost.bat
@@ -2,8 +2,7 @@
 if NOT "%GITHUB_WORKFLOW%" == "" (
     if EXIST "boost_1_78_0.7z" (
         "C:\Program Files\7-Zip\7z.exe" x boost_1_78_0.7z 
-        move boost_1_78_0 boost
-        cd boost
+        cd boost_1_78_0
         bootstrap.bat
         REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
@@ -13,8 +12,7 @@ if NOT "%GITHUB_WORKFLOW%" == "" (
 ) else (
     if EXIST "boost_1_78_0.7z" (
         .\bin\7z.exe x boost_1_78_0.7z 
-        move boost_1_78_0 boost
-        cd boost
+        cd boost_1_78_0
         bootstrap.bat
         REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
