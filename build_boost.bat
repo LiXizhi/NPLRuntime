36dedb4f33e70f745aa06bddb3e7bc21195082cb
wxaxiaoyao <765485868@qq.com>
2022-06-10 18:26:54 +0800

    update ci

--- build_boost.bat
+++ build_boost.bat
@@ -1,22 +1,24 @@
 
 if NOT "%GITHUB_WORKFLOW%" == "" (
     if EXIST "boost_1_78_0.7z" (
-        'C:\Program Files\7-Zip\7z.exe' x boost_1_78_0.7z 
-        REM cd boost_1_78_0
-        REM bootstrap.bat
-        REM REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-        REM REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-        REM b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-        REM b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-    )
-) else (
-    if EXIST "boost_1_78_0.7z" (
-        .\bin\7z.exe x boost_1_78_0.7z 
-        cd boost_1_78_0
+        "C:\Program Files\7-Zip\7z.exe" x boost_1_78_0.7z 
+        move boost_1_78_0 boost
+        cd boost
         bootstrap.bat
         REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-
+    )
+) else (
+    if EXIST "boost_1_78_0.7z" (
+        .\bin\7z.exe x boost_1_78_0.7z 
+        move boost_1_78_0 boost
+        cd boost
+        bootstrap.bat
+        REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+        REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+        b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+        b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+    )
 )
