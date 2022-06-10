34c023219b4eccd1de33dcde91311d45a60b2d92
wxaxiaoyao <765485868@qq.com>
2022-06-10 17:38:42 +0800

    update ci

--- build_boost.bat
+++ build_boost.bat
@@ -2,7 +2,8 @@
 if NOT "%GITHUB_WORKFLOW%" == "" (
     if EXIST "boost_1_78_0.7z" (
         "C:\Program Files\7-Zip\7z.exe" x boost_1_78_0.7z 
-        cd boost_1_78_0
+        move boost_1_78_0 boost
+        cd boost
         bootstrap.bat
         REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
@@ -12,10 +13,12 @@ if NOT "%GITHUB_WORKFLOW%" == "" (
 ) else (
     if EXIST "boost_1_78_0.7z" (
         .\bin\7z.exe x boost_1_78_0.7z 
-        cd boost_1_78_0
+        move boost_1_78_0 boost
+        cd boost
         bootstrap.bat
         REM b2 address-model=32 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         REM b2 address-model=64 link=static runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
         b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
+
 )
--- build_win32_github_workflows.bat
+++ build_win32_github_workflows.bat
@@ -4,27 +4,15 @@ REM git submodule update --init --recursive
 
 if NOT "%GITHUB_WORKFLOW%" == "" (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-
     mkdir build\win32
     cd build\win32
     cmake ..\..\Client -DNPLRUNTIME_PHYSICS=OFF -DBOOST_ROOT="%BOOST_ROOT%"
     cmake --build . -j 1
     REM msbuild .\CLIENT.sln /verbosity:minimal /property:Configuration=Release
 ) else (
-    if EXIST "boost_1_78_0.7z" (
-        .\bin\7z.exe x boost_1_78_0.7z 
-        cd boost_1_78_0
-        bootstrap.bat
-        b2 address-model=32 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-        b2 address-model=64 runtime-link=static threading=multi variant=release --with-thread --with-date_time --with-filesystem --with-system --with-chrono --with-serialization --with-iostreams --with-regex stage
-        set BOOST_ROOT=%cd%\boost_1_78_0
-        cd ..
-    )
-
     call "D:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
     mkdir build\win32
     cd build\win32
     cmake ..\..\Client -DUSE_OPENGL_RENDERER:BOOL=TRUE -DBOOST_ROOT="%BOOST_ROOT%"
     msbuild .\CLIENT.sln /verbosity:minimal /property:Configuration=Release
-
 )
