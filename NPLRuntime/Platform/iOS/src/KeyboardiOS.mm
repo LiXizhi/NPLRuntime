ef8252eab0860db64dcf7de275496a79bf5e4429
onedou <onedou@126.com>
2022-08-31 09:18:54 +0800

    fixed iOS keyboard

--- NPLRuntime/Platform/iOS/src/KeyboardiOS.mm
+++ NPLRuntime/Platform/iOS/src/KeyboardiOS.mm
@@ -10,6 +10,37 @@
 #include "Framework/InputSystem/VirtualKey.h"
 #include "2dengine/GUIRoot.h"
 #include "ParaEngineSettings.h"
+#include "util/StringHelper.h"
+
+@implementation KeyboardiOSControllerDelegate
+
+- (void)setKeyboardInstance:(KeyboardiOSController *)keyboardiOSController
+{
+    self.keyboardiOSController = keyboardiOSController;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (ParaEngine::CGlobals::GetApp()->GetAppState() == ParaEngine::PEAppState_Ready)
+    {
+        ParaEngine::CGUIRoot *pGUIRoot = ParaEngine::CGUIRoot::GetInstance();
+
+        if (pGUIRoot)
+        {
+            pGUIRoot->SendKeyDownEvent(ParaEngine::EVirtualKey::KEY_RETURN);
+            pGUIRoot->SendKeyUpEvent(ParaEngine::EVirtualKey::KEY_RETURN);
+        }
+    }
+
+    return NO;
+}
+
+- (BOOL)keyboardInputShouldDelete:(UITextField *)textField
+{
+    return true;
+}
+
+@end
 
 @implementation KeyboardiOSController
 
@@ -17,7 +48,6 @@ static KeyboardiOSController *instance = nil;
 static int mCtrlBottom = 0;
 static BOOL mUpdateViewSizeWhenKeyboardChange = NO;
 static UITextField *mTextField = nil;
-static NSString *mLastText = @"";
 
 + (void)InitLanguage
 {
@@ -80,19 +110,17 @@ static NSString *mLastText = @"";
 
     instance = [KeyboardiOSController alloc];
 
+    instance.keyboardiOSControllerDelegate = [[KeyboardiOSControllerDelegate alloc] init];
+
     mTextField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, -10, -10)];
     mTextField.delegate = appDelegate;
     mTextField.keyboardType = UIKeyboardTypeDefault;
     mTextField.userInteractionEnabled = NO;
+    
+    [instance.keyboardiOSControllerDelegate setKeyboardInstance:instance];
     [appDelegate.viewController.view addSubview:mTextField];
 
     instance.appDelegate = appDelegate;
-
-    [[NSNotificationCenter defaultCenter]
-        addObserver:instance
-        selector:@selector(keyboardOnPressed:)
-        name:UITextFieldTextDidBeginEditingNotification object:nil
-    ];
 
     [[NSNotificationCenter defaultCenter]
         addObserver:instance
@@ -122,15 +150,16 @@ static NSString *mLastText = @"";
     mUpdateViewSizeWhenKeyboardChange = bMoveView;
     mCtrlBottom = ctrlBottom;
 
-    if (bOpen && mTextField.userInteractionEnabled == NO)
-    {
+    if (bOpen) {
+        [NSTimer scheduledTimerWithTimeInterval:0.01 target:instance selector:@selector(keyboardOnPressed) userInfo:nil repeats:NO];
+    }
+
+    if (bOpen && mTextField.userInteractionEnabled == NO) {
         mTextField.enablesReturnKeyAutomatically = NO;
         mTextField.clearsOnBeginEditing = YES;
         mTextField.userInteractionEnabled = YES;
         [mTextField becomeFirstResponder];
-    }
-    else if (!bOpen && mTextField.userInteractionEnabled == YES)
-    {
+    } else if (!bOpen && mTextField.userInteractionEnabled == YES) {
         mTextField.userInteractionEnabled = NO;
         [mTextField resignFirstResponder];
     }
@@ -166,89 +195,40 @@ static NSString *mLastText = @"";
     }
 }
 
-- (void)keyboardOnPressed: (NSNotification*)notification
+- (void)keyboardOnPressed
 {
-    mTextField.text = @"-----------------------------------"; // length: 35
-    mLastText = @"";
+    ParaEngine::CGUIBase *pGUI = ParaEngine::CGUIRoot::GetInstance()->GetUIKeyFocus();
+
+    NSInteger curCaretPosition = pGUI->GetCaretPosition();
+    
+    std::string _curText;
+    pGUI->GetTextA(_curText);
+
+    NSString *curText = [NSString stringWithUTF8String:_curText.c_str()];
+    
+    mTextField.text = curText;
+    
+    UITextPosition *newPos = [mTextField positionFromPosition:mTextField.beginningOfDocument offset:curCaretPosition];
+    UITextRange *newRange = [mTextField textRangeFromPosition:newPos toPosition:newPos];
+    mTextField.selectedTextRange = newRange;
 }
 
 - (void)keyboardPressed:(NSNotification*)notification
 {
-    UITextInputMode *inputMode = [mTextField textInputMode];
-    NSString *inputLang = inputMode.primaryLanguage;
-
     if (ParaEngine::CGlobals::GetApp()->GetAppState() == ParaEngine::PEAppState_Ready)
     {
-        auto pGUI = ParaEngine::CGUIRoot::GetInstance()->GetUIKeyFocus();
-        auto pGUIIns = ParaEngine::CGUIRoot::GetInstance();
+        ParaEngine::CGUIBase *pGUI = ParaEngine::CGUIRoot::GetInstance()->GetUIKeyFocus();
+        ParaEngine::CGUIRoot *pGUIRoot = ParaEngine::CGUIRoot::GetInstance();
 
-        if ([mTextField.text length] < 35) {
-            pGUIIns->SendKeyDownEvent(ParaEngine::EVirtualKey::KEY_BACK);
-            pGUIIns->SendKeyUpEvent(ParaEngine::EVirtualKey::KEY_BACK);
-            mTextField.text = @"-----------------------------------";
-            return;
-        }
 
-        NSString *repText = [mTextField.text stringByReplacingOccurrencesOfString:@"-----------------------------------" withString:@""];
+        const char *curTextUTF8 = [mTextField.text UTF8String];
+        pGUI->SetTextA(curTextUTF8);
+        
+        
+        NSInteger setCaretPosition = [mTextField offsetFromPosition:mTextField.beginningOfDocument toPosition:[mTextField.selectedTextRange start]];
 
-        if ([inputLang isEqual:@"zh-Hans"]) {
-            NSUInteger count = [[NSRegularExpression
-               regularExpressionWithPattern:@"[A-Za-z]"
-               options:NSRegularExpressionCaseInsensitive
-               error:NULL]
-             numberOfMatchesInString:repText
-             options:NSMatchingProgress
-             range:NSMakeRange(0, [repText length])
-            ];
-
-            if (count > 0) {
-                return;
-            }
-        }
-
-        NSString *insertCharacter = [[NSRegularExpression
-            regularExpressionWithPattern:[NSString stringWithFormat:@"^%@", mLastText]
-            options:NSRegularExpressionUseUnicodeWordBoundaries
-            error:NULL]
-           stringByReplacingMatchesInString:repText
-           options:0
-           range:NSMakeRange(0, [repText length])
-           withTemplate:@""
-        ];
-
-        if ([repText length] > [mLastText length]) {
-            mLastText = repText;
-            unsigned long size = [insertCharacter length];
-
-            if (size >= 1) {
-                for (int i = 0; i < size; i++) {
-                    std::wstring s;
-                    s = (WCHAR)[insertCharacter characterAtIndex:i];
-                    pGUI->OnHandleWinMsgChars(s);
-                }
-            }
-        } else if ([repText length] < [mLastText length]) {
-            mLastText = repText;
-            pGUIIns->SendKeyDownEvent(ParaEngine::EVirtualKey::KEY_BACK);
-            pGUIIns->SendKeyUpEvent(ParaEngine::EVirtualKey::KEY_BACK);
-        }
+        pGUI->SetCaretPosition((int)setCaretPosition);
     }
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    if (textField == mTextField &&
-        ParaEngine::CGlobals::GetApp()->GetAppState() == ParaEngine::PEAppState_Ready)
-    {
-        auto pGUI = ParaEngine::CGUIRoot::GetInstance();
-        if (pGUI)
-        {
-            pGUI->SendKeyDownEvent(ParaEngine::EVirtualKey::KEY_RETURN);
-            pGUI->SendKeyUpEvent(ParaEngine::EVirtualKey::KEY_RETURN);
-        }
-    }
-
-    return NO;
 }
 
 @end
