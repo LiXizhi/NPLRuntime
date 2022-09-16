25d045d0603dc870466255f25e9ea79d48457cdc
huangyz <1451784145@qq.com>
2022-09-16 18:58:42 +0800

    同步dev异型cube优化(slope/stairs/slab)

--- NPLRuntime/ParaEngine/BlockEngine/BlockModelProvider.h
+++ NPLRuntime/ParaEngine/BlockEngine/BlockModelProvider.h
@@ -60,6 +60,13 @@ namespace ParaEngine
 		int GetModelCount() const;
 	protected:
 		int m_nModelCount;
+	protected:
+		/** 将一个点绕轴旋转
+		*/
+		Vector3 vec3Rotate(Vector3 &pt, Vector3 &angles);
+		/**将一个点绕点旋转
+		*/
+		Vector3 vec3RotateByPoint(Vector3 &originPt, Vector3 &pt, Vector3 &angles);
 	};
 
 }
