f5e6deeb17aecd187a640fea73f937a8296fefb5
tatfookpbb <wangben12ji@163.com>
2023-07-26 14:24:45 +0800

    去掉全面屏的导航栏，监听软键盘事件，发送键盘数据到引擎端

--- NPLRuntime/Platform/AndroidStudio/app/java/com/tatfook/paracraft/ParaEngineActivity.java
+++ NPLRuntime/Platform/AndroidStudio/app/java/com/tatfook/paracraft/ParaEngineActivity.java
@@ -646,6 +646,11 @@ public class ParaEngineActivity extends AppCompatActivity {
 
         this.hasFocus = hasFocus;
         resumeIfHasFocus();
+        //沉浸模式设置
+        if(hasFocus && Build.VERSION.SDK_INT>=19) {
+            View decorView=getWindow().getDecorView();
+            decorView.setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_STABLE |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |View.SYSTEM_UI_FLAG_FULLSCREEN |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     @Keep
