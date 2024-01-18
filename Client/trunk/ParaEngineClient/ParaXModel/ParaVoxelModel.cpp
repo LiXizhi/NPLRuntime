025ce17fd571c039ea47dc50c6781faed741366a
LiXizhi <lixizhi@yeah.net>
2024-01-18 20:21:14 +0800

    Voxel Model v0.1

--- Client/trunk/ParaEngineClient/ParaXModel/ParaVoxelModel.cpp
+++ Client/trunk/ParaEngineClient/ParaXModel/ParaVoxelModel.cpp
@@ -3,7 +3,9 @@
 // Authors:	LiXizhi
 // Emails:	LiXizhi@yeah.net
 // Date:	2024.1.17
-// local obj = entity:GetInnerObject():GetPrimaryAsset():GetAttributeObject():GetChildAt(0):GetChild("VoxelModel")
+// local model = entity:GetInnerObject():GetPrimaryAsset():GetAttributeObject():GetChildAt(0):GetChild("VoxelModel")
+// model:SetField("SetBlock", "7,7,7,8,254");
+// model:CallField("DumpOctree");
 //-----------------------------------------------------------------------------
 #include "ParaEngine.h"
 #include "ParaWorldAsset.h"
@@ -14,22 +16,45 @@
 #include "StringHelper.h"
 #include "ParaVoxelModel.h"
 
+// max octree depth is 12, which is 4096*4096*4096
+#define MAX_VOXEL_DEPTH 12
+
 using namespace ParaEngine;
+
+VoxelOctreeNode::VoxelOctreeNode()
+	: isBlockMask(0xff), colorRGB{ 0,0,0 }, baseChunkOffset(0), childMask(0xffffffffffffffff)
+{
+}
 
 ParaVoxelModel::ParaVoxelModel()
 {
-	m_chunks.push_back(VoxelChunk(1));
-	m_pRootNode = &(m_chunks[0][0]);
+	CreateGetFreeChunkIndex();
+	VoxelOctreeNode rootNode;
+	rootNode.isBlockMask = 0; // start as empty, or 0xff to start as full.
+	m_chunks[0]->push_back(rootNode);
 }
 
 ParaVoxelModel::~ParaVoxelModel()
 {
+	for (int i = 0; i < (int)m_chunks.size(); ++i)
+	{
+		delete m_chunks[i];
+	}
 	m_chunks.clear();
 }
+
+
+VoxelOctreeNode* ParaEngine::ParaVoxelModel::GetRootNode()
+{
+	return &((*m_chunks[0])[0]);
+}
+
 
 int ParaEngine::ParaVoxelModel::InstallFields(CAttributeClass* pClass, bool bOverride)
 {
 	IAttributeFields::InstallFields(pClass, bOverride);
+	pClass->AddField("SetBlock", FieldType_String, (void*)SetBlock_s, NULL, NULL, NULL, bOverride);
+	pClass->AddField("DumpOctree", FieldType_void, (void*)DumpOctree_s, NULL, NULL, NULL, bOverride);
 	return S_OK;
 }
 
@@ -44,11 +69,170 @@ bool ParaVoxelModel::Save(std::vector<char>& output)
 	return false;
 }
 
-void ParaVoxelModel::SetBlock(int x, int y, int z, int level, int color)
+
+inline int ParaEngine::ParaVoxelModel::LevelToDepth(int level)
 {
+	int nDepth = 0;
+	while (level > 1)
+	{
+		level >>= 1;
+		nDepth++;
+	}
+	return nDepth;
 }
 
-int ParaVoxelModel::GetBlock(int x, int y, int z, int level)
+int ParaEngine::ParaVoxelModel::CreateGetFreeChunkIndex(int nMinFreeSize)
+{
+	int nCount = m_chunks.size();
+	int nMinSize = 0xfe - nMinFreeSize;
+	for (int i = 0; i < nCount; ++i)
+	{
+		if ((int)m_chunks[i]->size() <= nMinSize)
+		{
+			return i;
+		}
+	}
+	VoxelChunk* chunk = new VoxelChunk();
+	chunk->reserve(255);
+	m_chunks.push_back(chunk);
+	return m_chunks.size() - 1;
+}
+
+VoxelOctreeNode* ParaEngine::ParaVoxelModel::CreateGetChildNode(VoxelOctreeNode* pNode, int nChildIndex)
+{
+	int nChildOffset = pNode->childOffsets[nChildIndex];
+	if (nChildOffset == 0xff)
+	{
+		// create a new child node
+		auto& chunk = *(m_chunks[pNode->baseChunkOffset]);
+		if(chunk.size() >= 254)
+		{
+			pNode->baseChunkOffset = CreateGetFreeChunkIndex();
+			auto& newChunk = *(m_chunks[pNode->baseChunkOffset]);
+
+			// create a new chunk and move all existing child nodes to the new chunk. 
+			for (int i = 0; i < 8; ++i)
+			{
+				if (pNode->childOffsets[i] != 0xff)
+				{
+					// move child to new chunk
+					newChunk.push_back(chunk[pNode->childOffsets[i]]);
+					pNode->childOffsets[i] = (uint8_t)(newChunk.size() - 1);
+					chunk.erase(chunk.begin() + pNode->childOffsets[i]);
+					newChunk.back().baseChunkOffset = pNode->baseChunkOffset;
+				}
+			}
+			newChunk.push_back(VoxelOctreeNode());
+			pNode->childOffsets[nChildIndex] = (uint8_t)(newChunk.size() - 1);
+			newChunk.back().baseChunkOffset = pNode->baseChunkOffset;
+			return &(newChunk.back());
+		}
+		else
+		{
+			// create in current chunk
+			chunk.push_back(VoxelOctreeNode());
+			pNode->childOffsets[nChildIndex] = (uint8_t)(chunk.size() - 1);
+			chunk.back().baseChunkOffset = pNode->baseChunkOffset;
+			return &(chunk.back());
+		}
+	}
+	else
+	{
+		return &((*m_chunks[pNode->baseChunkOffset])[nChildOffset]);
+	}
+}
+
+VoxelOctreeNode* ParaEngine::ParaVoxelModel::GetChildNode(VoxelOctreeNode* pNode, int nChildIndex)
+{
+	int nChildOffset = pNode->childOffsets[nChildIndex];
+	if (nChildOffset == 0xff)
+	{
+		return NULL;
+	}
+	else
+	{
+		return &((*m_chunks[pNode->baseChunkOffset])[nChildOffset]);
+	}
+}
+
+void ParaEngine::ParaVoxelModel::DumpOctree()
+{
+	OUTPUT_LOG("dumping ParaVoxelModel %d chunks:\n", (int)m_chunks.size());
+	VoxelOctreeNode* pNode = GetRootNode();
+	DumpOctreeNode(pNode, 0, 0, 0);
+}
+
+void ParaEngine::ParaVoxelModel::DumpOctreeNode(VoxelOctreeNode* pNode, int nDepth, int nChunkIndex, int offset)
+{
+	char tmp[256];
+	StringHelper::fast_sprintf(tmp, "Node[%d][%d]: baseChunkOffset %d, blockMask: %d color: %2x%2x%2x\n", nChunkIndex, offset, pNode->baseChunkOffset, pNode->isBlockMask, pNode->colorRGB[0], pNode->colorRGB[1], pNode->colorRGB[2]);
+	OUTPUT_LOG(tmp);
+
+	if(pNode->IsLeaf())
+		return;
+	for (int i = 0; i < nDepth; i++)
+		tmp[i] = '-';
+	tmp[nDepth] = '\0';
+	for (int k = 0; k < 8; ++k)
+	{
+		OUTPUT_LOG("%s", tmp);
+		if (pNode->childOffsets[k] != 0xff)
+		{
+			OUTPUT_LOG("%d: ", k);
+			DumpOctreeNode(GetChildNode(pNode, k), nDepth + 1, pNode->baseChunkOffset, pNode->childOffsets[k]);
+		}
+		else
+		{
+			OUTPUT_LOG("%d: None\n", k);
+		}
+	}
+}
+
+void ParaVoxelModel::SetBlock(uint32 x, uint32 y, uint32 z, int level, int color)
+{
+	int nDepth = LevelToDepth(level);
+	// create get octree node
+	VoxelOctreeNode* pNode = GetRootNode();
+	
+	nDepth--;
+	if (color > 0)
+	{
+		VoxelOctreeNode* parentNodes[MAX_VOXEL_DEPTH];
+		parentNodes[0] = pNode;
+		// create or set block
+		int nChildIndex = 0;
+		int nLevel = 1;
+		for(;nDepth >= 0; nDepth--, nLevel++)
+		{
+			uint32 lx = x >> nDepth, ly = y >> nDepth, lz = z >> nDepth;
+			nChildIndex = (lx & 1) + ((ly & 1) << 1) + ((lz & 1) << 2);
+			// pNode->isBlockMask |= (1 << nChildIndex);
+			pNode = CreateGetChildNode(pNode, nChildIndex);
+			parentNodes[nLevel] = pNode;
+		}
+		pNode->colorRGB[0] = (color & 0xff0000) >> 16;
+		pNode->colorRGB[1] = (color & 0x00ff00) >> 8;
+		pNode->colorRGB[2] = color & 0x0000ff;
+		UpdateNode(parentNodes, nLevel);
+	}
+	else
+	{
+		// delete block
+	}
+}
+
+void ParaEngine::ParaVoxelModel::SetBlockCmd(const char* cmd)
+{
+	uint32 x, y, z;
+	int level;
+	int color;
+	if (sscanf(cmd, "%d,%d,%d,%d,%d", &x, &y, &z, &level, &color) == 5)
+	{
+		SetBlock(x, y, z, level, color);
+	}
+}
+
+int ParaVoxelModel::GetBlock(uint32 x, uint32 y, uint32 z, int level)
 {
 	return 0;
 }
@@ -60,6 +244,49 @@ bool ParaVoxelModel::RayPicking(const Vector3& origin, const Vector3& dir, Vecto
 
 void ParaVoxelModel::Optimize()
 {
+
+}
+
+void ParaEngine::ParaVoxelModel::OptimizeNode(VoxelOctreeNode* pNode)
+{
+	if (pNode->IsLeaf())
+		return;
+	
+	int solidCount = 0;
+	uint8_t isBlockMask = 0;
+	for (int k = 0; k < 8; ++k)
+	{
+		auto pChild = GetChildNode(pNode, k);
+		if (pChild)
+		{
+			OptimizeNode(pChild);
+			if(pChild->IsBlock())
+				isBlockMask |= (1 << k);
+		}
+	}
+	pNode->isBlockMask = isBlockMask;
+}
+
+void ParaEngine::ParaVoxelModel::UpdateNode(VoxelOctreeNode* nodes[], int nNodeCount)
+{
+	for (int i = nNodeCount - 1; i >= 0; --i)
+	{
+		auto pNode = nodes[i];
+		if (pNode->IsLeaf()) {
+			continue;
+		}
+		uint8_t isBlockMask = 0;
+		for (int k = 0; k < 8; ++k)
+		{
+			auto pChild = GetChildNode(pNode, k);
+			if (pChild)
+			{
+				if (pChild->IsBlock())
+					isBlockMask |= (1 << k);
+			}
+		}
+		pNode->isBlockMask = isBlockMask;
+	}
 }
 
 void ParaVoxelModel::Draw(SceneState* pSceneState)
--- Client/trunk/ParaEngineClient/ParaXModel/ParaVoxelModel.h
+++ Client/trunk/ParaEngineClient/ParaXModel/ParaVoxelModel.h
@@ -9,15 +9,34 @@ namespace ParaEngine
 	/** a voxel octree data node
 	* 1(isBlockMask)+3(color)+4(base)+8(children) = 16 bytes
 	* a chunk contains at most 254 nodes = 16*254 = 4064 bytes ~= 4KB
+	* 
+	* if it is a leaf node or there are over 4 blocks in the 8 children, we will consider it as a block-typed node.
+	* block are rendered as cube at its level of detail regardless of its completely full or only half full. 
 	*/
-	struct VoxelData
+	struct VoxelOctreeNode
 	{
+		VoxelOctreeNode();
+
 		// 8 bits isBlockMask whether the 8 spaces in octree are blocks. 
 		uint8_t isBlockMask;
 		// 24 bits for color
 		uint8_t colorRGB[3];
 		uint32_t baseChunkOffset;
-		uint8_t childOffsets[8];
+		union {
+			uint8_t childOffsets[8];
+			uint64_t childMask;
+		};
+		inline bool IsLeaf() { return childMask == 0xffffffffffffffff; };
+		inline int GetBlockCountInMask() { 
+			int count = 0;
+			uint8_t n = isBlockMask;
+			while (n) {
+				n &= (n - 1);
+				count++;
+			}
+			return count;
+		};
+		inline int IsBlock() { return IsLeaf() || (GetBlockCountInMask() >= 4); }
 	};
 #pragma pack(pop)
 
@@ -33,6 +52,9 @@ namespace ParaEngine
 
 		ATTRIBUTE_DEFINE_CLASS(ParaVoxelModel);
 
+		ATTRIBUTE_METHOD1(ParaVoxelModel, SetBlock_s, char*) { cls->SetBlockCmd(p1); return S_OK; }
+		ATTRIBUTE_METHOD(ParaVoxelModel, DumpOctree_s) { cls->DumpOctree(); return S_OK; }
+		
 		/** this class should be implemented if one wants to add new attribute. This function is always called internally.*/
 		virtual int InstallFields(CAttributeClass* pClass, bool bOverride);
 			
@@ -46,30 +68,54 @@ namespace ParaEngine
 		/** set the block at the given position. 
 		* @param x, y, z : the position of the block relative to level.
 		* @param level: the level or model width. this should be a power of 2, like 1, 2, 4, 8, 16, ..., 1024, ...
-		* @param color: the 8 bits color of the block. -1 if the block is empty.
+		* @param color: the 24 bits color of the block. -1 if the block is empty.
 		*/
-		void SetBlock(int x, int y, int z, int level, int color);
+		void SetBlock(uint32 x, uint32 y, uint32 z, int level, int color);
+		/**
+		* @param cmd: the command string to set the block. 
+		* e.g. "7,7,7,8,254" means set the block at (7,7,7) at level 8 to color 254.
+		*/
+		void SetBlockCmd(const char* cmd);
 
 		/** get the block color at the given position and level
 		* @return -1 if the block is empty. or return 8 bits color of the block.
 		*/
-		int GetBlock(int x, int y, int z, int level);
+		int GetBlock(uint32 x, uint32 y, uint32 z, int level);
 
 		/** ray picking at given level
 		* @param level: the model level at which to pick. this should be a power of 2, like 1, 2, 4, 8, 16, ..., 1024, ...
 		*/
 		bool RayPicking(const Vector3& origin, const Vector3& dir, Vector3& hitPos, int& hitColor, int level = -1);
 
-		/** optimize the model for faster rendering. */
-		void Optimize();
-
 		/** draw blocks with dynamic level of detail with reference to the camera position in scene state.
 		*/
 		void Draw(SceneState* pSceneState);
+		
+	public:
+		/** optimize the model to remove and merge octree node. */
+		void Optimize();
+		void OptimizeNode(VoxelOctreeNode* pNode);
+		/** 
+		* @param nodes: update nodes from nodes[nNodeCount - 1](smallest child) to nodes[0] (root node). 
+		*/
+		void UpdateNode(VoxelOctreeNode* nodes[], int nNodeCount);
 
+		/** get the depth of the octree at the given level. 
+		* e.g. LevelToDepth(1024) == 10
+		*/
+		inline int LevelToDepth(int level);
+
+		int CreateGetFreeChunkIndex(int nMinFreeSize = 8);
+
+		VoxelOctreeNode* CreateGetChildNode(VoxelOctreeNode* pNode, int nChildIndex);
+		VoxelOctreeNode* GetChildNode(VoxelOctreeNode* pNode, int nChildIndex);
+
+		void DumpOctree();
+		void DumpOctreeNode(VoxelOctreeNode* pNode, int nDepth, int nChunkIndex, int offset);
+
+		VoxelOctreeNode* GetRootNode();
 	private:
-		typedef std::vector<VoxelData> VoxelChunk;
-		std::vector< VoxelChunk > m_chunks;
-		VoxelData* m_pRootNode;
+		typedef std::vector<VoxelOctreeNode> VoxelChunk;
+		std::vector< VoxelChunk* > m_chunks;
 	};
 }
