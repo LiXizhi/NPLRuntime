6687f50c6e0325424eddc6b904e6cfca6cf127f4
xiaoyao <765485868@qq.com>
2024-01-24 17:10:44 +0800

    update cpp to lua

--- Client/trunk/externals/antlr/src/ParaAntlr4.cpp
+++ Client/trunk/externals/antlr/src/ParaAntlr4.cpp
@@ -3,7 +3,7 @@
 // Authors:	wuxiangan
 // Emails:
 // Date: 2023.10.19
-// Desc: 
+// Desc:
 //----------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
@@ -18,22 +18,38 @@ using namespace antlr4;
 
 std::string ParaPythonToLua(std::string python_code)
 {
-    ANTLRInputStream input(python_code.data(), python_code.size());
-    Python3Lexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    Python3Parser parser(&tokens);
-    Python3ToLuaVisitor visitor;
-    std::any result = visitor.visitFile_input(parser.file_input());
-    return std::any_cast<std::string>(result);
+    try
+    {
+        ANTLRInputStream input(python_code.data(), python_code.size());
+        Python3Lexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        Python3Parser parser(&tokens);
+        Python3ToLuaVisitor visitor;
+        std::any result = visitor.visitFile_input(parser.file_input());
+        return std::any_cast<std::string>(result);
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "ParaPythonToLua: " << e.what() << std::endl;
+        return "";
+    }
 }
 
 std::string ParaCppToLua(std::string cpp_code)
 {
-    ANTLRInputStream input(cpp_code.data(), cpp_code.size());
-    CPP14Lexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    CPP14Parser parser(&tokens);
-    CPP14ToLuaVisitor visitor;
-    std::any result = visitor.visitTranslationUnit(parser.translationUnit());
-    return std::any_cast<std::string>(result);
+    try
+    {
+        ANTLRInputStream input(cpp_code.data(), cpp_code.size());
+        CPP14Lexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        CPP14Parser parser(&tokens);
+        CPP14ToLuaVisitor visitor;
+        std::any result = visitor.visitTranslationUnit(parser.translationUnit());
+        return std::any_cast<std::string>(result);
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "ParaCppToLua: " << e.what() << std::endl;
+        return "";
+    }
 }
