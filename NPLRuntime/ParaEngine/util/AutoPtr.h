291b19589eb7023d2d96ae082bba8348cd0fdbcf
LiXizhi <lixizhi@yeah.net>
2024-03-07 15:46:58 +0800

    auto ptr class removed

--- NPLRuntime/ParaEngine/util/AutoPtr.h
+++ NPLRuntime/ParaEngine/util/AutoPtr.h
@@ -1,30 +1,36 @@
-﻿#ifndef __AUTO_PTR_H__
-#define __AUTO_PTR_H__
+﻿#pragma once
 
-#ifdef EMSCRIPTEN_SINGLE_THREAD
 #include <memory>
 namespace std {
-    template<typename T>
-    class auto_ptr
-    {
-    public:
-        auto_ptr(T* p = 0): m_ptr(p) {}
-        auto_ptr(auto_ptr& r): m_ptr(std::move(r.m_ptr)) {}
-        T* get() const  { return m_ptr.get(); }
-        T& operator*() const { return *m_ptr; }
-        T* operator->() const { return m_ptr.get(); }
-        void reset( T* p = 0 ) { m_ptr.reset(p); }
-        T* release() { return m_ptr.release(); }
-    private:
-        std::unique_ptr<T> m_ptr;
-    };
+
+	// A std::unique_ptr with move sematics for single-threaded environment
+	// since std::auto_ptr is deprecated in C++11, use this one instead
+	template<typename T>
+	class movable_auto_ptr
+	{
+	public:
+		movable_auto_ptr(T* p = 0) : m_ptr(p) {}
+		movable_auto_ptr(const movable_auto_ptr& other) : m_ptr(std::move(const_cast<movable_auto_ptr&>(other).m_ptr)) {}
+		movable_auto_ptr(movable_auto_ptr&& r) : m_ptr(std::move(r.m_ptr)) {}
+		movable_auto_ptr& operator=(movable_auto_ptr&& r) {
+			if (this != &r) {
+				m_ptr = std::move(r.m_ptr);
+			}
+			return *this;
+		}
+		inline T* get() const { return m_ptr.get(); }
+		inline T& operator*() const { return *m_ptr; }
+		inline T* operator->() const { return m_ptr.get(); }
+		inline void reset(T* p = 0) { m_ptr.reset(p); }
+		inline T* release() { return m_ptr.release(); }
+	private:
+		std::unique_ptr<T> m_ptr;
+	};
 }
 
 namespace boost {
-    template<class T> T * get_pointer(std::auto_ptr<T> const& p)
-    {
-        return p.get();
-    }
+	template<class T> T* get_pointer(std::movable_auto_ptr<T> const& p)
+	{
+		return p.get();
+	}
 }
-#endif
-#endif
