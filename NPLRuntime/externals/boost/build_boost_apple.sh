b99c5104ab9433be1f2866c1d28c4c30f08ee4ea
onedou <onedou@126.com>
2024-04-16 09:39:49 +0800

    updated build boost architectures on apple.

--- NPLRuntime/externals/boost/build_boost_apple.sh
+++ NPLRuntime/externals/boost/build_boost_apple.sh
@@ -51,7 +51,7 @@ TVOS_SDK_VERSION=`xcrun --sdk appletvos --show-sdk-version`
 MIN_MACOS_VERSION=10.10
 MACOS_SDK_VERSION=`xcrun --sdk macosx --show-sdk-version | cut -d. -f1 -f2`
 
-MACOS_ARCHS="x86_64"
+MACOS_ARCHS="x86_64 arm64"
 MACOS_ARCH_COUNT=0
 MACOS_ARCH_FLAGS=""
 
