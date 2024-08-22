0fbee22beba3c43c4251526a5061469986b9cc50
xiaoyao <765485868@qq.com>
2024-08-22 14:02:55 +0800

    auto keyup

--- NPLRuntime/Platform/SDL/src/RenderWindowDelegate.cpp
+++ NPLRuntime/Platform/SDL/src/RenderWindowDelegate.cpp
@@ -17,6 +17,43 @@ ParaEngine::IRenderWindow *CreateParaRenderWindow(const int width, const int hei
 namespace ParaEngine
 {
 
+	static std::unordered_map<EVirtualKey, uint64_t> s_keydown_map;
+	static EVirtualKey s_ctrl_shift_alt_keys[] = {EVirtualKey::KEY_LCONTROL, EVirtualKey::KEY_RCONTROL, EVirtualKey::KEY_LSHIFT, EVirtualKey::KEY_RSHIFT, EVirtualKey::KEY_LMENU, EVirtualKey::KEY_RMENU};
+	static int s_ctrl_shift_alt_keys_size = sizeof(s_ctrl_shift_alt_keys) / sizeof(s_ctrl_shift_alt_keys[0]);
+
+	static void RefreshKeyDownMap()
+	{
+		static uint64_t last_refresh_timestamp = 0;
+		uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+		if (timestamp - last_refresh_timestamp < 1000)
+		{
+			return;
+		}
+
+		last_refresh_timestamp = timestamp;
+		for (auto it = s_keydown_map.begin(); it != s_keydown_map.end();)
+		{
+			int timeout = 2000;
+			for (int i = 0; i < s_ctrl_shift_alt_keys_size; i++)
+			{
+				if (s_ctrl_shift_alt_keys[i] == it->first)
+				{
+					timeout = 5000;
+				}
+			}
+
+			if ((it->second + timeout) < timestamp)
+			{
+				CGUIRoot::GetInstance()->SendKeyUpEvent(it->first);
+				it = s_keydown_map.erase(it);
+			}
+			else
+			{
+				it++;
+			}
+		}
+	}
+
 	void RenderWindowDelegate::OnTouch(int nType, TouchEvent::TouchEventMsgType nTouchType, int touch_id, float x, float y, int nTimeTick)
 	{
 		if (CGlobals::GetApp()->GetAppState() != PEAppState_Ready)
@@ -66,19 +103,33 @@ namespace ParaEngine
 		CGUIRoot::GetInstance()->GetMouse()->PushMouseEvent(DeviceMouseEventPtr(new DeviceMouseWheelEvent(delta)));
 	}
 
+	void RenderWindowDelegate::PollEvents()
+	{
+		RefreshKeyDownMap();
+		RenderWindowSDL2::PollEvents();
+	}
+
 	void RenderWindowDelegate::OnKey(EVirtualKey key, EKeyState state)
 	{
-		static std::unordered_map<EVirtualKey, uint64_t> s_keydown_map;
-		uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();;		
 		if (CGlobals::GetApp()->GetAppState() != PEAppState_Ready)
 		{
 			return;
 		}
+		uint64_t timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
 		bool pressed = state == EKeyState::PRESS ? true : false;
 		if (pressed)
 		{
 			CGUIRoot::GetInstance()->SendKeyDownEvent(key);
 			s_keydown_map.insert_or_assign(key, timestamp);
+
+			for (int i = 0; i < s_ctrl_shift_alt_keys_size; i++)
+			{
+				auto i_key = s_ctrl_shift_alt_keys[i];
+				if (s_keydown_map.find(i_key) != s_keydown_map.end())
+				{
+					s_keydown_map.insert_or_assign(i_key, timestamp);
+				}
+			}
 		}
 		else
 		{
@@ -86,18 +137,7 @@ namespace ParaEngine
 			s_keydown_map.erase(key);
 		}
 
-		for (auto it = s_keydown_map.begin(); it != s_keydown_map.end(); )
-		{
-			if ((it->second + 2000) < timestamp)
-			{
-				CGUIRoot::GetInstance()->SendKeyUpEvent(it->first);
-				it = s_keydown_map.erase(it);
-			}
-			else
-			{
-				it++;
-			}
-		}
+		RefreshKeyDownMap();
 	}
 
 	void RenderWindowDelegate::OnChar(unsigned int character)
@@ -115,17 +155,19 @@ namespace ParaEngine
 		}
 	}
 
-	void RenderWindowDelegate::OnClearChar(std::string text) 
+	void RenderWindowDelegate::OnClearChar(std::string text)
 	{
 		auto pGUI = CGUIRoot::GetInstance()->GetUIKeyFocus();
-		if (pGUI) pGUI->SetTextA("");
+		if (pGUI)
+			pGUI->SetTextA("");
 		OnChar(text);
 	}
 
 	void RenderWindowDelegate::OnChar(std::string sTextUTF8)
 	{
 		auto app = CGlobals::GetApp();
-		if (app->GetAppState() != PEAppState_Ready) return;
+		if (app->GetAppState() != PEAppState_Ready)
+			return;
 
 		auto pGUI = CGUIRoot::GetInstance()->GetUIKeyFocus();
 		if (pGUI)
@@ -177,7 +219,7 @@ namespace ParaEngine
 				return;
 			}
 		}
-		
+
 		std::cout << "OnSize" << " w: " << w << " h: " << h << std::endl;
 		if (w > 0 && h > 0)
 		{
