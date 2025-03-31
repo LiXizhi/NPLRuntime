2aebfb815133e932cf156a7a6b9afa8feed3a268
LiXizhi <lixizhi@yeah.net>
2025-03-31 20:22:27 +0800

    minor fix web socket

--- NPLRuntime/ParaEngine/NPL/NPLConnection.cpp
+++ NPLRuntime/ParaEngine/NPL/NPLConnection.cpp
@@ -838,6 +838,7 @@ bool NPL::CNPLConnection::handle_websocket_data(int bytes_transferred)
 					NPL::NPLHelper::EncodeStringInQuotation(m_input_msg.m_code, 0, (const char*)(&m_websocket_input_data[0]), (int)m_websocket_input_data.size());
 					handleMessageIn();
 				}
+				break;
 			}
 			case NPL::WebSocket::CLOSE:
 				stop();
