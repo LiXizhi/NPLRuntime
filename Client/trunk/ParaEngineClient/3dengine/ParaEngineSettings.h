c091e164e4aa53bca2becc59b11b31df497bcd39
xiaoyao <765485868@qq.com>
2025-04-07 17:37:44 +0800

    add srceen rotated interface

--- Client/trunk/ParaEngineClient/3dengine/ParaEngineSettings.h
+++ Client/trunk/ParaEngineClient/3dengine/ParaEngineSettings.h
@@ -241,6 +241,7 @@ namespace ParaEngine
 
 		ATTRIBUTE_METHOD1(ParaEngineSettings, GetLandscapeMode_s, const char**) { *p1 = cls->GetLandscapeMode().c_str(); return S_OK; }
 		ATTRIBUTE_METHOD1(ParaEngineSettings, SetLandscapeMode_s, const char*) { cls->SetLandscapeMode(p1); return S_OK; }
+        ATTRIBUTE_METHOD1(ParaEngineSettings, IsScreenRotated_s, bool*) { *p1 = cls->IsScreenRotated(); return S_OK; }
 
 		ATTRIBUTE_METHOD1(ParaEngineSettings, GetRendererName_s, const char**) { *p1 = cls->GetRendererName(); return S_OK; }
 
@@ -746,6 +747,7 @@ namespace ParaEngine
 
 		const std::string& GetLandscapeMode();
         void SetLandscapeMode(const char* mode);
+        bool IsScreenRotated();
 
 		/** @return "OpenGL" or "DirectX" */
 		const char* GetRendererName();
