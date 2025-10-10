cfd8078e1e1f88ce6ce2a72aaf0e312ad93b00bb
LiXizhi <lixizhi@yeah.net>
2025-10-10 16:12:13 +0800

    Add temporary directory and mount for IDBFS in main function

--- NPLRuntime/Platform/SDL/emscripten/main.cpp
+++ NPLRuntime/Platform/SDL/emscripten/main.cpp
@@ -246,6 +246,8 @@ int main(int argc, char* argv[])
         FS.mount(IDBFS, { root: '/worlds' }, '/worlds');
         FS.mkdir('/Database');
         FS.mount(IDBFS, { root: '/Database' }, '/Database');
+        FS.mkdir('/temp');
+        FS.mount(IDBFS, { root: '/temp' }, '/temp');
         FS.syncfs(true, function(err) {
             console.log("加载IDBFS!!!");
             Module._emscripten_filesystem_inited();
